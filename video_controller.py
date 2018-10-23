c663562210b783091696f08be1dfa63e604de240
Luc Sinet <luc.sinet@etixgroup.com>
2018-10-23 19:28:41 +0200

    develop #comment Add youtube playlist parser

--- video_controller.py
+++ video_controller.py
@@ -7,8 +7,28 @@ omx_player = omxplayer.make_player()
 def stream_video(url, sub=False):
     omx_player.stop()
 
+    if '/playlist' in url:
+        urls = parse_playlist(url)
+        url = urls[0]
+        add_urls_to_queue(urls[1:])
+
     video_path = download_video(url)
     omx_player.play(video_path)
+
+
+def parse_playlist(url):
+    ydl_opts = {
+        'ignoreerrors': True,
+        'extract_flat': 'in_playlist'
+    }
+    ydl = youtube_dl.YoutubeDL(ydl_opts)
+    with ydl:  # Download the playlist data without downloading the videos.
+        data = ydl.extract_info(url, download=False)
+
+    base_url = url.split('/playlist', 1)[0]
+    urls = [base_url + '/watch?v=' + entry['id'] for entry in data['entries']]
+
+    return urls
 
 
 def download_video(url):
@@ -26,4 +46,9 @@ def download_video(url):
     return video_path
 
 
-# stream_video("https://www.youtube.com/playlist?list=PLlpsJiGuS_nOn6Qvl35ik4X1AN7b7WlhM")
+def add_urls_to_queue(urls):
+    for url in urls:
+        print("url= ", url)
+
+
+stream_video("https://www.youtube.com/playlist?list=PLlpsJiGuS_nOn6Qvl35ik4X1AN7b7WlhM")
