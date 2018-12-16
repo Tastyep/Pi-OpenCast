74035a236fd2d179009b81b18faa4a641ac77afa
Luc Sinet <luc.sinet@etixgroup.com>
2018-12-16 10:31:26 +0100

    develop #comment Implement TestCase object which adds more helpers to the standard one.

---
+++ test/util.py
@@ -0,0 +1,10 @@
+# Allow direct execution
+import unittest
+
+
+class TestCase(unittest.TestCase):
+    def assertEmpty(self, o):
+        self.assertFalse(len(o) > 0)
+
+    def assertNonEmpty(self, o):
+        self.assertTrue(len(o) > 0)
