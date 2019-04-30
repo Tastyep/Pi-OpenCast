9d74531b79e69f1b6dfb09865526650a39dc6ef6
Luc Sinet <luc.sinet@etixgroup.com>
2019-04-30 10:23:40 +0200

    develop #comment Upgrade pyyaml to 5.1

--- OpenCast/__init__.py
+++ OpenCast/__init__.py
@@ -12,7 +12,7 @@ def _real_main(argv):
     app_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
     with open('{}/logging.yml'.format(app_path), 'r') as file:
-        cfg = yaml.load(file)
+        cfg = yaml.load(file, Loader=yaml.Loader)
         logging.config.dictConfig(cfg)
 
     config.load_from_file('{}/config.ini'.format(app_path))
--- test/__init__.py
+++ test/__init__.py
@@ -6,5 +6,5 @@ import logging.config
 if "OPENCAST_ENABLE_LOGGING" in os.environ:
     app_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     with open('{}/logging.yml'.format(app_path), 'r') as file:
-        cfg = yaml.load(file)
+        cfg = yaml.load(file, Loader=yaml.Loader)
         logging.config.dictConfig(cfg)
