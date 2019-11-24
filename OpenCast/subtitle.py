77573b42653fe8c3f7905682189b4b7a48b5e3af
Luc Sinet <luc.sinet@etixgroup.com>
2019-11-24 18:54:10 +0100

    develop #comment Extract softcoded subtitles when available.

--- OpenCast/subtitle.py
+++ OpenCast/subtitle.py
@@ -1,18 +1,45 @@
 import logging
 from pathlib import Path
 
+import ffmpeg
+
+from .config import config
+
 logger = logging.getLogger(__name__)
+config = config['Subtitle']
 
 
-def load_from_video_path(video_path):
-    path = Path(video_path)
+def load_from_video_path(path):
     video_name = path.name.rsplit('.', 1)[0]
-    video_path = path.parents[0]
+    parent_path = path.parents[0]
+    subtitle = '{}/{}.srt'.format(parent_path, video_name)
 
-    srtFiles = list(video_path.glob('*.srt'))
-    matchingSubs = [str(srt) for srt in srtFiles if video_name in str(srt)]
+    logger.debug("[subtitle] loading subtitles for {}".format(path))
+    srtFiles = list(parent_path.glob('*.srt'))
+    if Path(subtitle) in srtFiles:
+        logger.debug(
+            "[subtitle] found matching susbtitle file: {}".format(subtitle)
+        )
+        return subtitle
 
-    return matchingSubs
+    logger.debug(
+        "[subtitle] searching softcoded subtitles from {}".format(path)
+    )
+    probe = ffmpeg.probe(str(path))
+    for stream in probe['streams']:
+        logger.debug("sub: {}".format(stream))
+        if stream['codec_type'] == 'subtitle' and stream['tags'][
+            'language'] == config.language:
+            channel = '0:{}'.format(stream['index'])
+            logger.info("[subtitle] found matching sub: {}".format(subtitle))
+            try:
+                ffmpeg.input(str(path)).output(
+                    subtitle, map=channel
+                ).global_args('-n').run()
+                return subtitle
+            except ffmpeg.Error as e:
+                if e is None:  # The file probably exists
+                    return subtitle
+                logger.error("[subtitle] extraction error: {}".format(e.stderr))
 
-    # NOTE: Add future support for a subtitle_path config entry.
-    # subtitle_path = ['./subtitle/', '/Video/subtitles']
+    return None
