dac098d9b5f8c62ff38d4134dbf3696689b3ac34
Luc Sinet <luc.sinet@etixgroup.com>
2019-12-13 15:09:26 +0100

    develop #comment Refactor application setup.

--- OpenCast/__init__.py
+++ OpenCast/__init__.py
@@ -4,8 +4,14 @@ import yaml
 import logging
 import logging.config
 
-from .server import Server
 from .config import config
+from .infra.io.server import Server
+from .infra.io import video_downloader
+from .infra.media import player_wrapper
+
+from .app.app_facade import AppFacade
+from .app.controller.module import ControllerModule
+from .app.service.module import ServiceModule
 
 
 def _real_main(argv):
@@ -14,14 +20,21 @@ def _real_main(argv):
     with open('{}/logging.yml'.format(app_path), 'r') as file:
         cfg = yaml.load(file, Loader=yaml.Loader)
         logging.config.dictConfig(cfg)
+    logger = logging.getLogger(__name__)
 
     config.load_from_file('{}/config.ini'.format(app_path))
+    serverConfig = config['Server']
 
-    s = Server()
+    app_facade = AppFacade()
+    server = Server()
+    player = player_wrapper.make_wrapper()
+    downloader = video_downloader.make_video_downloader()
+    service_module = ServiceModule(app_facade, player, downloader)
+    controller_module = ControllerModule(app_facade, app_path, server)
+
     try:
-        s.run()
+        server.run(serverConfig.host, serverConfig.port)
     except Exception as e:
-        logger = logging.getLogger(__name__)
         logger.debug("opencast stopped: {}".format(str(e)))
 
 
