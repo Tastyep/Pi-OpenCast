65f8f2b2c2c41fdbff882aaefc53bb3521c182d7
Luc Sinet <luc.sinet@gmail.com>
2020-04-04 20:29:16 +0200

    ddd-migration #comment PlayerController -> PlayerMonitController

---
+++ OpenCast/app/controller/player_monit_controller.py
@@ -0,0 +1,88 @@
+import logging
+import uuid
+
+from bottle import request
+from OpenCast.domain.model.player import Player
+
+from ..command import player_commands as c
+from .controller import Controller
+
+logger = logging.getLogger(__name__)
+
+
+class PlayerMonitController(Controller):
+    def __init__(self, app_facade, data_facade, io_facade):
+        super(PlayerMonitController, self).__init__(app_facade)
+
+        self._player_repo = data_facade.player_repo()
+        self._player_repo.create(Player(uuid.uuid4()))
+        self._video_repo = data_facade.video_repo()
+
+        # TODO add video monit controller
+        server = io_facade.server()
+        server.route("/stream", callback=self._stream)
+        server.route("/queue", callback=self._queue)
+        server.route("/video", callback=self._video)
+        server.route("/subtitle", callback=self._subtitle)
+        server.route("/sound", callback=self._sound)
+        server.route("/running", callback=self._running)
+
+    def _stream(self):
+        cmd = c.PlayVideo(request.query["url"])
+        self._cmd_dispatcher.dispatch(cmd)
+        return "1"
+
+    def _queue(self):
+        cmd = c.QueueVideo(request.query["url"])
+        self._cmd_dispatcher.dispatch(cmd)
+
+    def _video(self):
+        control = request.query["control"]
+        cmd = None
+
+        logger.debug("Control command received: {}".format(control))
+        if control == "pause":
+            cmd = c.ToggleVideoState()
+        elif control == "stop":
+            cmd = c.StopVideo()
+        elif control == "right":
+            cmd = c.SeekVideo(30)
+        elif control == "left":
+            cmd = c.SeekVideo(-30)
+        elif control == "longright":
+            cmd = c.SeekVideo(300)
+        elif control == "longleft":
+            cmd = c.SeekVideo(-300)
+        elif control == "prev":
+            cmd = c.PrevVideo()
+        elif control == "next":
+            cmd = c.NextVideo()
+        else:
+            return "0"
+
+        self._cmd_dispatcher.dispatch(cmd)
+        return "1"
+
+    def _sound(self):
+        cmd = None
+        if request.query["vol"] == "more":
+            cmd = c.ChangeVolume(10)
+        else:
+            cmd = c.ChangeVolume(-10)
+
+        self._cmd_dispatcher.dispatch(cmd)
+        return "1"
+
+    def _subtitle(self):
+        action = request.query["action"]
+
+        logger.debug("Subtitle command received: {}".format(action))
+        if action == "toggle":
+            self._cmd_dispatcher.dispatch(c.ToggleSubtitle())
+        elif action == "increase":
+            self._cmd_dispatcher.dispatch(c.IncreaseSubtitleDelay())
+        elif action == "decrease":
+            self._cmd_dispatcher.dispatch(c.DecreaseSubtitleDelay())
+
+    def _running(self):
+        return 1
