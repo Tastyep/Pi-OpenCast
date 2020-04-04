ab363b555016a9d36751b2fde871b5e799369261
Luc Sinet <luc.sinet@gmail.com>
2020-04-04 20:41:04 +0200

    ddd-migration #comment Define the data module.
     - The data module is in charge of handling data stored by the application (ex: repositories).

---
+++ OpenCast/infra/data/data_facade.py
@@ -0,0 +1,10 @@
+class DataFacade(object):
+    def __init__(self, repo_factory):
+        self._player_repo = repo_factory.make_player_repo()
+        self._video_repo = repo_factory.make_video_repo()
+
+    def player_repo(self):
+        return self._player_repo
+
+    def video_repo(self):
+        return self._video_repo
