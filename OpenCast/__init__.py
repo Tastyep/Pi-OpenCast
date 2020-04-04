fae404041ac10386c9fe513064cb4cbd57cb632e
Luc Sinet <luc.sinet@gmail.com>
2020-04-04 21:23:56 +0200

    ddd-migration #comment Rework the setup of the application to initialize the different layers independently.

--- OpenCast/__init__.py
+++ OpenCast/__init__.py
@@ -1,45 +1,56 @@
-import sys
-import os
-import yaml
 import logging
 import logging.config
+import os
+import sys
 
-from .config import config
-from .infra.io.server import Server
-from .infra.io import video_downloader
-from .infra.media import player_wrapper
+import yaml
 
 from .app.app_facade import AppFacade
 from .app.controller.module import ControllerModule
 from .app.service.module import ServiceModule
+from .config import config
+from .infra.data.data_facade import DataFacade
+from .infra.data.repo.factory import RepoFactory
+from .infra.io.io_facade import IoFacade
+from .infra.io.io_factory import IoFactory
+from .infra.media.media_facade import MediaFacade
+from .infra.media.media_factory import MediaFactory
 
 
-def _real_main(argv):
+def _real_main():
     app_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-    with open('{}/logging.yml'.format(app_path), 'r') as file:
+    with open("{}/logging.yml".format(app_path), "r") as file:
         cfg = yaml.load(file, Loader=yaml.Loader)
         logging.config.dictConfig(cfg)
     logger = logging.getLogger(__name__)
 
-    config.load_from_file('{}/config.ini'.format(app_path))
-    serverConfig = config['Server']
+    config.load_from_file("{}/config.ini".format(app_path))
+    server_config = config["Server"]
 
     app_facade = AppFacade()
-    server = Server()
-    player = player_wrapper.make_wrapper()
-    downloader = video_downloader.make_video_downloader()
-    service_module = ServiceModule(app_facade, player, downloader)
-    controller_module = ControllerModule(app_facade, server)
+
+    repo_factory = RepoFactory()
+    data_facade = DataFacade(repo_factory)
+
+    io_factory = IoFactory()
+    io_facade = IoFacade(io_factory)
+
+    media_factory = MediaFactory(app_facade.evt_dispatcher())
+    media_facade = MediaFacade(media_factory)
+
+    controller_module = ControllerModule(app_facade, data_facade, io_facade)
+    service_module = ServiceModule(app_facade, data_facade, io_facade, media_facade)
 
     try:
-        server.run(serverConfig.host, serverConfig.port)
+        server = io_facade.server()
+        server.run(server_config.host, server_config.port)
     except Exception as e:
         logger.debug("opencast stopped: {}".format(str(e)))
 
 
 def main(argv=None):
     try:
-        _real_main(argv)
+        _real_main()
     except KeyboardInterrupt:
-        sys.exit('\nERROR: Interrupted by user')
+        sys.exit("\nERROR: Interrupted by user")
