9a1703627549c794deb11ef85b034696cf4597be
Luc Sinet <luc.sinet@gmail.com>
2020-04-04 20:32:20 +0200

    ddd-migration #comment Pass facades to app controllers.

--- OpenCast/app/controller/file_controller.py
+++ OpenCast/app/controller/file_controller.py
@@ -1,19 +1,17 @@
-import os
-
 from .controller import Controller
 
 
 class FileController(Controller):
-    def __init__(self, cmd_dispatcher, server):
-        super(FileController, self).__init__(cmd_dispatcher)
-        self._server = server
+    def __init__(self, app_facade, io_facade):
+        super(FileController, self).__init__(app_facade)
+        self._server = io_facade.server()
         # Todo hardcoded port
-        self._index_html = '''
+        self._index_html = """
                                 {{ project_name }} API.
                                 Please visit :8081
-                           '''
+                           """
 
-        server.route('/', callback=self._index)
+        self._server.route("/", callback=self._index)
 
     def _index(self):
-        return self._server.template(self._index_html, project_name='OpenCast')
+        return self._server.template(self._index_html, project_name="OpenCast")
--- OpenCast/app/controller/module.py
+++ OpenCast/app/controller/module.py
@@ -1,10 +1,10 @@
 from .file_controller import FileController
-from .player_controller import PlayerController
+from .player_monit_controller import PlayerMonitController
 
 
 class ControllerModule(object):
-    def __init__(self, app_facade, server):
-        cmd_dispatcher = app_facade.cmd_dispatcher()
-
-        self._file_controller = FileController(cmd_dispatcher, server)
-        self._player_controller = PlayerController(cmd_dispatcher, server)
+    def __init__(self, app_facade, data_facade, io_facade):
+        self._file_controller = FileController(app_facade, io_facade)
+        self._player_monit_controller = PlayerMonitController(
+            app_facade, data_facade, io_facade
+        )
