452d93fd8cd1571fbd16f06f2afec4b8e315864e
Luc Sinet <luc.sinet@gmail.com>
2020-04-26 10:24:40 +0200

    ddd-migration #comment Add the video service, responsible for handling video related commands.

--- OpenCast/app/service/module.py
+++ OpenCast/app/service/module.py
@@ -1,10 +1,12 @@
 from .player import PlayerService
+from .video import VideoService
 
 
 class ServiceModule:
     def __init__(
         self, app_facade, data_facade, io_facade, media_facade, service_factory
     ):
-        self._player_service = PlayerService(
-            app_facade, data_facade, io_facade, media_facade, service_factory
+        self._player_service = PlayerService(app_facade, data_facade, media_facade)
+        self._video_service = VideoService(
+            app_facade, data_facade, io_facade, service_factory
         )
