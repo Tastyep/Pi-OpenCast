b53c0967bdb958ac8f42b06e9eb0b2841a9d8519
Luc Sinet <luc.sinet@gmail.com>
2020-05-06 14:40:54 +0200

    develop #comment Allow the event dispatcher to observe multiple events in one call (grouped observation).
    This allows to observe the event that should result from a command as well as the new OperationError event.

--- OpenCast/domain/event/dispatcher.py
+++ OpenCast/domain/event/dispatcher.py
@@ -5,67 +5,76 @@ import structlog
 
 class EventDispatcher:
     class _Handler:
-        def __init__(self, functor, count):
-            self._functor = functor
+        def __init__(self, evtcls_to_handler, count):
+            self._evtcls_to_handler = evtcls_to_handler
             self._count = count
 
-        def __call__(self, *args):
-            self._functor(*args)
+        def __call__(self, evt, *args):
+            self._evtcls_to_handler[type(evt)](evt, *args)
+
+        def handled_evts(self):
+            return self._evtcls_to_handler.keys()
+
+        def update_expires(self):
             if self._count > 0:
                 self._count -= 1
 
-        def expired(self):
             return self._count == 0
 
     def __init__(self, executor):
         self._logger = structlog.get_logger(__name__)
         self._executor = executor
-        self._handlers_map = {}
+        self._handler_map = {}
+        self._evt_to_handler_ids = {}
         self._lock = Lock()
 
-    def observe(self, evt_cls, handler, times=-1):
-        self._observe(evt_cls, None, self._Handler(handler, times))
-
     def once(self, evt_cls, handler):
-        self.observe(evt_cls, handler, 1)
+        self.observe(None, {evt_cls: handler}, 1)
 
-    def observe_id(self, evt_cls, evt_id, handler, times=-1):
-        self._observe(evt_cls, evt_id, self._Handler(handler, times))
+    def observe(self, evt_id, evtcls_to_handler: dict, times=-1):
+        self._observe(
+            evt_id, evtcls_to_handler.keys(), self._Handler(evtcls_to_handler, times)
+        )
 
     def dispatch(self, evt):
         def impl(evt):
             self._logger.debug(type(evt).__name__, evt=evt)
 
-            evt_cls = type(evt)
+            def remove_handler_links(evt_id, handler):
+                handler_id = id(handler)
+                for evt_cls in handler.handled_evts():
+                    evt_hash = hash((evt_id, evt_cls))
+                    self._evt_to_handler_ids[evt_hash].remove(handler_id)
+                self._handler_map.pop(handler_id)
+
             handlers = []
             with self._lock:
-                if evt_cls not in self._handlers_map:
-                    return
-
-                evt_handlers_map = self._handlers_map[evt_cls]
                 for evt_id in set([None, evt.id]):
-                    if evt_id in evt_handlers_map:
-                        handlers = [*handlers, *evt_handlers_map[evt_id]]
+                    evt_hash = hash((evt_id, type(evt)))
+                    if evt_hash not in self._evt_to_handler_ids:
+                        continue
+
+                    handler_ids = self._evt_to_handler_ids[evt_hash]
+                    for handler_id in handler_ids:
+                        handler = self._handler_map[handler_id]
+                        handlers.append(handler)
+                        if handler.update_expires():
+                            remove_handler_links(evt_id, handler)
 
             if not handlers:
                 return
             for handler in handlers:
                 handler(evt)
 
-            with self._lock:
-                evt_handlers_map = self._handlers_map[evt_cls]
-                for evt_id in set([None, evt.id]):
-                    if evt_id in evt_handlers_map:
-                        for handler in list(evt_handlers_map[evt_id]):
-                            if handler.expired():
-                                evt_handlers_map[evt_id].remove(handler)
-
         self._executor.submit(impl, evt)
 
-    def _observe(self, evt_cls, evt_id, handler):
-        if evt_cls not in self._handlers_map:
-            self._handlers_map[evt_cls] = {evt_id: []}
-        id_to_handlers = self._handlers_map.get(evt_cls, {evt_id: []})
-        handlers = id_to_handlers.get(evt_id, [])
-        handlers.append(handler)
-        self._handlers_map[evt_cls][evt_id] = handlers
+    def _observe(self, evt_id, evt_clss: list, handler):
+        with self._lock:
+            handler_id = id(handler)
+            self._handler_map[handler_id] = handler
+
+            for evt_cls in evt_clss:
+                evt_hash = hash((evt_id, evt_cls))
+                handler_ids = self._evt_to_handler_ids.get(evt_hash, [])
+                handler_ids.append(handler_id)
+                self._evt_to_handler_ids[evt_hash] = handler_ids
