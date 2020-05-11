212125de1ad9f77979dffdac006f8e7aa12e9b01
Luc Sinet <luc.sinet@gmail.com>
2020-05-11 18:49:40 +0200

    develop #comment Add fixture for model tests

--- test/domain/model/test_video.py
+++ test/domain/model/test_video.py
@@ -1,10 +1,10 @@
-from test.util import TestCase
-
 import OpenCast.domain.event.video as Evt
 from OpenCast.domain.model.video import Video
 
+from .util import ModelTestCase
 
-class VideoTest(TestCase):
+
+class VideoTest(ModelTestCase):
     def test_construction(self):
         video = Video(None, "source", None)
         self.assertEqual(None, video.playlist_id)
@@ -12,9 +12,7 @@ class VideoTest(TestCase):
         self.assertEqual(None, video.title)
         self.assertEqual(None, video.path)
         self.assertEqual(None, video.subtitle)
-
-        video_evts = video.release_events()
-        self.assertListEqual([Evt.VideoCreated], video_evts.keys())
+        self.expect_events(video, Evt.VideoCreated)
 
     def make_video(self):
         video = Video(None, "source", None)
@@ -25,19 +23,19 @@ class VideoTest(TestCase):
     def test_identify(self):
         video = self.make_video()
         video.title = "name"
-        self.assertListEqual([Evt.VideoIdentified], video.release_events().keys())
+        self.expect_events(video, Evt.VideoIdentified)
 
     def test_retrieve(self):
         video = self.make_video()
         video.path = "/tmp"
-        self.assertListEqual([Evt.VideoRetrieved], video.release_events().keys())
+        self.expect_events(video, Evt.VideoRetrieved)
 
     def test_set_subtitles(self):
         video = self.make_video()
         video.subtitle = "/tmp/toto.srt"
-        self.assertListEqual([Evt.VideoSubtitleFetched], video.release_events().keys())
+        self.expect_events(video, Evt.VideoSubtitleFetched)
 
     def test_delete(self):
         video = self.make_video()
         video.delete()
-        self.assertListEqual([Evt.VideoDeleted], video.release_events().keys())
+        self.expect_events(video, Evt.VideoDeleted)
