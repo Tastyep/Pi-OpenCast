674e94c54ea5b644fa3897ec282e6adb426fd3f3
Luc Sinet <luc.sinet@gmail.com>
2020-05-12 11:48:41 +0200

    develop #comment Add skeleton of domain services tests.

---
+++ test/domain/service/test_subtitle.py
@@ -0,0 +1,11 @@
+from test.util import TestCase
+
+from OpenCast.domain.service.subtitle import SubtitleService
+
+
+class SubtitleServiceTest(TestCase):
+    def setUp(self):
+        self._service = SubtitleService(None)
+
+    def test_load_from_disk(self):
+        pass
