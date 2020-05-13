f691d5d39d008e320e791c1e4fbf11ba4f0d797d
Luc Sinet <luc.sinet@gmail.com>
2020-05-13 20:39:37 +0200

    develop #comment Simplify command dispatching from controllers.

--- OpenCast/app/controller/controller.py
+++ OpenCast/app/controller/controller.py
@@ -6,9 +6,6 @@ class Controller:
         self._cmd_dispatcher = app_facade.cmd_dispatcher()
         self._evt_dispatcher = app_facade.evt_dispatcher()
 
-    def _make_cmd(self, cmd_cls, component_id, *args, **kwargs):
+    def _dispatch(self, cmd_cls, component_id, *args, **kwargs):
         cmd_id = IdentityService.id_command(cmd_cls, component_id)
-        return cmd_cls(cmd_id, component_id, *args, **kwargs)
-
-    def _dispatch(self, cmd, *args, **kwargs):
-        self._cmd_dispatcher.dispatch(cmd(*args, **kwargs))
+        self._cmd_dispatcher.dispatch(cmd_cls(cmd_id, component_id, *args, **kwargs))
--- OpenCast/app/controller/player_monitor.py
+++ OpenCast/app/controller/player_monitor.py
@@ -82,73 +82,68 @@ class PlayerMonitController(Controller):
                 self._cmd_dispatcher,
                 self._evt_dispatcher,
                 self._video_repo,
-                self._source_service,
+                videos,
             )
-            workflow.start(video_ids, sources, playlist_id)
+            workflow.start()
             return
 
         video_id = IdentityService.id_video(source)
+        video = Video(video_id, source, None)
         workflow_id = IdentityService.id_workflow(QueueVideoWorkflow, video_id)
         workflow = QueueVideoWorkflow(
             workflow_id,
             self._cmd_dispatcher,
             self._evt_dispatcher,
             self._video_repo,
-            self._source_service,
+            video,
         )
-        workflow.start(video_id, source, playlist_id=None)
+        workflow.start()
 
     def _video(self):
         control = request.query["control"]
-        cmd = None
-
         if control == "pause":
-            cmd = self._make_cmd(Cmd.ToggleVideoState)
+            self._dispatch(Cmd.ToggleVideoState)
         elif control == "stop":
-            cmd = self._make_cmd(Cmd.StopVideo)
+            self._dispatch(Cmd.StopVideo)
         elif control == "right":
-            cmd = self._make_cmd(Cmd.SeekVideo, 30)
+            self._dispatch(Cmd.SeekVideo, 30)
         elif control == "left":
-            cmd = self._make_cmd(Cmd.SeekVideo, -30)
+            self._dispatch(Cmd.SeekVideo, -30)
         elif control == "longright":
-            cmd = self._make_cmd(Cmd.SeekVideo, 300)
+            self._dispatch(Cmd.SeekVideo, 300)
         elif control == "longleft":
-            cmd = self._make_cmd(Cmd.SeekVideo, -300)
+            self._dispatch(Cmd.SeekVideo, -300)
         elif control == "prev":
-            cmd = self._make_cmd(Cmd.PrevVideo)
+            self._dispatch(Cmd.PrevVideo)
         elif control == "next":
-            cmd = self._make_cmd(Cmd.NextVideo)
+            self._dispatch(Cmd.NextVideo)
         else:
             return "0"
 
-        self._cmd_dispatcher.dispatch(cmd)
         return "1"
 
     def _sound(self):
-        cmd = None
         if request.query["vol"] == "more":
-            cmd = self._make_cmd(Cmd.ChangeVolume, 10)
+            self._dispatcher(Cmd.ChangeVolume, 10)
         else:
-            cmd = self._make_cmd(Cmd.ChangeVolume, -10)
-
-        self._cmd_dispatcher.dispatch(cmd)
+            self._dispatcher(Cmd.ChangeVolume, -10)
         return "1"
 
     def _subtitle(self):
         action = request.query["action"]
 
         if action == "toggle":
-            self._cmd_dispatcher.dispatch(self._make_cmd(Cmd.ToggleSubtitle))
+            self._dispatch(Cmd.ToggleSubtitle)
         elif action == "increase":
-            self._cmd_dispatcher.dispatch(self._make_cmd(Cmd.IncreaseSubtitleDelay))
+            self._dispatch(Cmd.IncreaseSubtitleDelay)
         elif action == "decrease":
-            self._cmd_dispatcher.dispatch(self._make_cmd(Cmd.DecreaseSubtitleDelay))
+            self._dispatch(Cmd.DecreaseSubtitleDelay)
 
     def _running(self):
         return 1
 
-    def _make_cmd(self, cmd_cls, *args, **kwargs):
+    def _dispatch(self, cmd_cls, *args, **kwargs):
         player_id = IdentityService.id_player()
-        return super(PlayerMonitController, self)._make_cmd(
+        return super(PlayerMonitController, self)._dispatch(
             cmd_cls, player_id, *args, **kwargs
         )
