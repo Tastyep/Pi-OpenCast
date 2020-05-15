7e67082c0341b60e1e7effb1af340f9cec426eba
Luc Sinet <luc.sinet@gmail.com>
2020-05-15 18:45:25 +0200

    develop #comment Add tests for StreamPlaylistWorkflow.

--- test/app/workflow/test_player.py
+++ test/app/workflow/test_player.py
@@ -5,6 +5,7 @@ import OpenCast.domain.event.player as Evt
 from OpenCast.app.workflow.player import (
     QueuePlaylistWorkflow,
     QueueVideoWorkflow,
+    StreamPlaylistWorkflow,
     StreamVideoWorkflow,
     Video,
     VideoWorkflow,
@@ -176,3 +177,87 @@ class StreamVideoWorkflowTest(WorkflowTestCase):
             self.workflow, Evt.PlayerStarted, cmd.id, self.player_id, self.video.id,
         )
         self.assertTrue(self.workflow.is_COMPLETED())
+
+
+class StreamPlaylistWorkflowTest(WorkflowTestCase):
+    def setUp(self):
+        super(StreamPlaylistWorkflowTest, self).setUp()
+        self.video_repo = Mock()
+        self.video_repo.exists.return_value = True
+
+    def make_test_workflow(self, video_count=2):
+        playlist_id = IdentityService.id_playlist(f"playlist{video_count}")
+        sources = [f"src{i}" for i in range(video_count)]
+        videos = [
+            Video(IdentityService.id_video(source), source, playlist_id)
+            for source in sources
+        ]
+        return self.make_workflow(StreamPlaylistWorkflow, self.video_repo, videos)
+
+    def test_initial(self):
+        workflow = self.make_test_workflow()
+        self.assertTrue(workflow.is_INITIAL())
+
+    def test_init_to_playing(self):
+        workflow = self.make_test_workflow()
+        self.expect_workflow_creation(StreamVideoWorkflow)
+        workflow.start()
+        self.assertTrue(workflow.is_PLAYING())
+
+    def test_playing_to_queueing_from_completed(self):
+        workflow = self.make_test_workflow()
+        play_workflow = self.expect_workflow_creation(StreamVideoWorkflow)
+        workflow.to_PLAYING(None)
+
+        self.expect_workflow_creation(QueueVideoWorkflow)
+        self.raise_event(workflow, play_workflow.Completed, workflow.id)
+        self.assertTrue(workflow.is_QUEUEING())
+
+    def test_playing_to_playing_from_aborted(self):
+        workflow = self.make_test_workflow()
+        play_workflow = self.expect_workflow_creation(StreamVideoWorkflow)
+        workflow.to_PLAYING(None)
+        self.raise_event(workflow, play_workflow.Aborted, workflow.id)
+        self.assertTrue(workflow.is_PLAYING())
+
+    def test_playing_to_completed_from_completed(self):
+        workflow = self.make_test_workflow(video_count=1)
+        play_workflow = self.expect_workflow_creation(StreamVideoWorkflow)
+        workflow.to_PLAYING(None)
+        self.raise_event(workflow, play_workflow.Completed, workflow.id)
+        self.assertTrue(workflow.is_COMPLETED())
+
+    def test_playing_to_completed_from_aborted(self):
+        workflow = self.make_test_workflow(video_count=1)
+        play_workflow = self.expect_workflow_creation(StreamVideoWorkflow)
+        workflow.to_PLAYING(None)
+        self.raise_event(workflow, play_workflow.Aborted, workflow.id)
+        self.assertTrue(workflow.is_COMPLETED())
+
+    def test_queueing_to_queueing_from_completed(self):
+        workflow = self.make_test_workflow()
+        queue_workflow = self.expect_workflow_creation(QueueVideoWorkflow)
+        workflow.to_QUEUEING(None)
+        self.raise_event(workflow, queue_workflow.Completed, workflow.id)
+        self.assertTrue(workflow.is_QUEUEING())
+
+    def test_queueing_to_queueing_from_aborted(self):
+        workflow = self.make_test_workflow()
+        queue_workflow = self.expect_workflow_creation(QueueVideoWorkflow)
+        workflow.to_QUEUEING(None)
+        self.raise_event(workflow, queue_workflow.Aborted, workflow.id)
+        self.assertTrue(workflow.is_QUEUEING())
+
+    def test_queueing_to_completed_from_completed(self):
+        workflow = self.make_test_workflow(video_count=1)
+        queue_workflow = self.expect_workflow_creation(QueueVideoWorkflow)
+        workflow.to_QUEUEING(None)
+        self.raise_event(workflow, queue_workflow.Completed, workflow.id)
+        self.assertTrue(workflow.is_COMPLETED())
+
+    def test_queueing_to_completed_from_aborted(self):
+        workflow = self.make_test_workflow(video_count=1)
+        queue_workflow = self.expect_workflow_creation(QueueVideoWorkflow)
+        workflow.to_QUEUEING(None)
+        self.raise_event(workflow, queue_workflow.Aborted, workflow.id)
+        self.assertTrue(workflow.is_COMPLETED())
