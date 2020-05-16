a66f989ec9b612571a255bb7d81a972bd163eeeb
Luc Sinet <luc.sinet@gmail.com>
2020-05-16 14:30:33 +0200

    develop #comment Add missing self attribute to factory method.

--- OpenCast/app/workflow/factory.py
+++ OpenCast/app/workflow/factory.py
@@ -10,17 +10,17 @@ from .video import VideoWorkflow
 class WorkflowFactory(object):
     """Creates instances of workflows."""
 
-    def make_video_workflow(*args, **kwargs):
+    def make_video_workflow(self, *args, **kwargs):
         return VideoWorkflow(*args, **kwargs)
 
-    def make_queue_video_workflow(*args, **kwargs):
+    def make_queue_video_workflow(self, *args, **kwargs):
         return QueueVideoWorkflow(*args, **kwargs)
 
-    def make_queue_playlist_video_workflow(*args, **kwargs):
+    def make_queue_playlist_video_workflow(self, *args, **kwargs):
         return QueuePlaylistWorkflow(*args, **kwargs)
 
-    def make_stream_video_workflow(*args, **kwargs):
+    def make_stream_video_workflow(self, *args, **kwargs):
         return StreamVideoWorkflow(*args, **kwargs)
 
-    def make_stream_playlist_video_workflow(*args, **kwargs):
+    def make_stream_playlist_video_workflow(self, *args, **kwargs):
         return StreamPlaylistWorkflow(*args, **kwargs)
