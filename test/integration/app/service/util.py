bc21f1fc1a32745db389ccdac82a4f817f9e3997
Luc Sinet <luc.sinet@gmail.com>
2020-05-22 11:09:10 +0200

    develop #comment Adds integration tests for the video service.

---
+++ test/integration/app/service/util.py
@@ -0,0 +1,32 @@
+from concurrent.futures import ThreadPoolExecutor
+from test.integration.app.event_expecter import EventExpecter
+from test.shared.infra.data.producer import DataProducer
+from test.shared.infra.facade_mock import InfraFacadeMock
+from test.util import TestCase
+from unittest.mock import Mock
+
+from OpenCast.app.facade import AppFacade
+from OpenCast.domain.service.factory import ServiceFactory
+from OpenCast.infra.data.facade import DataFacade
+from OpenCast.infra.data.repo.factory import RepoFactory
+
+
+class ServiceTestCase(TestCase):
+    def setUp(self):
+        def execute_handler(handler, *args):
+            handler(*args)
+
+        app_executor = Mock()
+        app_executor.submit = Mock(side_effect=execute_handler)
+        self.app_facade = AppFacade(app_executor)
+
+        repo_factory = RepoFactory()
+        self.data_facade = DataFacade(repo_factory)
+
+        self.service_factory = ServiceFactory()
+
+        self.infra_facade = InfraFacadeMock()
+        self.evt_expecter = EventExpecter(
+            self.app_facade.cmd_dispatcher, self.app_facade.evt_dispatcher
+        )
+        self.data_producer = DataProducer()
