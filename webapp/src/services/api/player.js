a9c3b8ed86063159acfae00c25bbe691866bb038
Luc Sinet <luc.sinet@gmail.com>
2020-06-11 12:15:24 +0200

    react-webapp #comment Define the remaining api routes.

--- webapp/src/services/api/player.js
+++ webapp/src/services/api/player.js
@@ -8,4 +8,37 @@ async function queueMedia(url) {
   return await API.get("player/queue", { params: { url: url } });
 }
 
-export default { streamMedia, queueMedia };
+async function nextMedia() {
+  return await API.get("player/next");
+}
+
+async function prevMedia() {
+  return await API.get("player/prev");
+}
+
+async function stopMedia() {
+  return await API.get("player/stop");
+}
+
+async function pauseMedia() {
+  return await API.get("player/pause");
+}
+
+async function seekMedia(duration) {
+  return await API.get("player/seek", { params: { duration: duration } });
+}
+
+async function updateVolume(value) {
+  return await API.get("player/volume", { params: { value: value } });
+}
+
+export default {
+  streamMedia,
+  queueMedia,
+  nextMedia,
+  prevMedia,
+  stopMedia,
+  pauseMedia,
+  seekMedia,
+  updateVolume,
+};
