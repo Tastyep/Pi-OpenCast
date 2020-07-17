26e53765b0978af0eb027e4fcf195cc1283a6db9
Luc Sinet <luc.sinet@gmail.com>
2020-07-17 14:58:45 +0200

    api-cleanup #comment Merge next & prev into pick video.

--- OpenCast/app/controller/player_monitor.py
+++ OpenCast/app/controller/player_monitor.py
@@ -17,7 +17,7 @@ from .monitor import MonitorController
 
 class PlayerMonitController(MonitorController):
     def __init__(self, app_facade, infra_facade, data_facade, service_factory):
-        super().__init__(app_facade, infra_facade)
+        super().__init__(app_facade, infra_facade, "/player")
 
         media_factory = infra_facade.media_factory
         self._source_service = service_factory.make_source_service(
@@ -28,19 +28,23 @@ class PlayerMonitController(MonitorController):
         self._video_repo = data_facade.video_repo
         self._player_repo.create(Player(uuid.uuid4()))
 
-        self._route("player/stream", callback=self._stream)
-        self._route("player/queue", callback=self._queue)
-        self._route("player/next", callback=self._next)
-        self._route("player/prev", callback=self._prev)
-        self._route("player/stop", callback=self._stop)
-        self._route("player/pause", callback=self._pause)
-        self._route("player/seek", callback=self._seek)
-        self._route("player/volume", callback=self._volume)
-        self._route("player/subtitle/toggle", callback=self._subtitle_toggle)
-        self._route("player/subtitle/seek", callback=self._subtitle_seek)
+        self._route("/", method="GET", callback=self._get)
+        self._route("/stream", method="POST", callback=self._stream)
+        self._route("/queue", method="POST", callback=self._queue)
+        self._route("/video", method="POST", callback=self._pick_video)
+        self._route("/stop", method="POST", callback=self._stop)
+        self._route("/pause", method="POST", callback=self._pause)
+        self._route("/seek", method="POST", callback=self._seek)
+        self._route("/volume", method="POST", callback=self._volume)
+        self._route("/subtitle/toggle", method="POST", callback=self._subtitle_toggle)
+        self._route("/subtitle/seek", method="POST", callback=self._subtitle_seek)
 
-    def _stream(self, request):
-        source = request.query["url"]
+    def _get(self, _):
+        player = self._player_repo.get_player()
+        return self._make_response(200, player.to_json())
+
+    def _stream(self, req):
+        source = req.query["url"]
         if self._source_service.is_playlist(source):
             sources = self._source_service.unfold(source)
             playlist_id = IdentityService.id_playlist(source)
@@ -60,8 +64,8 @@ class PlayerMonitController(MonitorController):
 
         return "1"
 
-    def _queue(self, request):
-        source = request.query["url"]
+    def _queue(self, req):
+        source = req.query["url"]
         if self._source_service.is_playlist(source):
             sources = self._source_service.unfold(source)
             playlist_id = IdentityService.id_playlist(source)
@@ -79,11 +83,9 @@ class PlayerMonitController(MonitorController):
         video = Video(video_id, source, None)
         self._start_workflow(QueueVideoWorkflow, video_id, self._video_repo, video)
 
-    def _next(self, _):
-        self._dispatch(Cmd.NextVideo)
-
-    def _prev(self, _):
-        self._dispatch(Cmd.PrevVideo)
+    def _pick_video(self, req):
+        video_id = uuid.UUID(req.query["video_id"])
+        self._dispatch(Cmd.PickVideo, video_id)
 
     def _stop(self, _):
         self._dispatch(Cmd.StopPlayer)
@@ -91,22 +93,22 @@ class PlayerMonitController(MonitorController):
     def _pause(self, _):
         self._dispatch(Cmd.ToggleVideoState)
 
-    def _seek(self, request):
-        forward = str_to_bool(request.query["forward"])
-        long = str_to_bool(request.query["long"])
+    def _seek(self, req):
+        forward = str_to_bool(req.query["forward"])
+        long = str_to_bool(req.query["long"])
         side = 1 if forward is True else -1
         step = Player.LONG_TIME_STEP if long is True else Player.SHORT_TIME_STEP
         self._dispatch(Cmd.SeekVideo, side * step)
 
-    def _volume(self, request):
-        volume = int(request.query["value"])
+    def _volume(self, req):
+        volume = int(req.query["value"])
         self._dispatch(Cmd.ChangeVolume, volume)
 
     def _subtitle_toggle(self, _):
         self._dispatch(Cmd.ToggleSubtitle)
 
-    def _subtitle_seek(self, request):
-        forward = str_to_bool(request.query["forward"])
+    def _subtitle_seek(self, req):
+        forward = str_to_bool(req.query["forward"])
         step = Player.SUBTITLE_DELAY_STEP if forward else -Player.SUBTITLE_DELAY_STEP
         self._dispatch(Cmd.IncreaseSubtitleDelay, step)
 
