b9b57c6a76c1b428b7aea41c3e711e763cbef746
Luc Sinet <luc.sinet@gmail.com>
2020-07-21 18:45:15 +0200

    api-cleanup #comment Replace bottle with aiohttp.
     After trying Flask it seems that an async server is required for handling sse.
     This will allow us to avoid combining flask + gevenv.

--- OpenCast/infra/io/server.py
+++ OpenCast/infra/io/server.py
@@ -1,55 +1,29 @@
+import aiohttp_cors as cors
 import structlog
-from bottle import Bottle, HTTPResponse, request, response, run, template
-
-
-class EnableCors(object):
-    name = "enable_cors"
-    api = 2
-
-    _allow_origin = "*"
-    _allow_methods = "PUT, GET, POST, DELETE, OPTIONS"
-    _allow_headers = (
-        "Authorization, Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
-    )
-
-    def apply(self, fn, context):
-        def _enable_cors(*args, **kwargs):
-            # set CORS headers
-            response.headers["Access-Control-Allow-Origin"] = self._allow_origin
-            response.headers["Access-Control-Allow-Methods"] = self._allow_methods
-            response.headers["Access-Control-Allow-Headers"] = self._allow_headers
-
-            if request.method != "OPTIONS":
-                # actual request; reply with the actual response
-                return fn(request, *args, **kwargs)
-
-        return _enable_cors
+from aiohttp import web
 
 
 class Server:
     def __init__(self):
-        self._server = Bottle()
+        self._app = web.Application()
         self._logger = structlog.get_logger(__name__)
-        self._enable_cors()
+        self._cors = cors.setup(
+            self._app,
+            defaults={
+                "*": cors.ResourceOptions(
+                    allow_credentials=True, expose_headers="*", allow_headers="*",
+                )
+            },
+        )
 
-    def route(self, route, *args, **kwargs):
-        self._server.route(route, *args, **kwargs)
+    def route(self, method, route, handle):
+        route = self._app.router.add_route(method, route, handle)
+        self._cors.add(route)
 
     def run(self, host, port):
         self._logger.info(f"Started", host=host, port=port)
 
-        run(self._server, host=host, port=port, reloader=False, debug=True, quiet=True)
+        web.run_app(self._app, host=host, port=port)
 
-    def template(self, *args, **kwargs):
-        return template(*args, **kwargs)
-
-    def make_response(self, status, body):
-        return HTTPResponse(body=body, status=status)
-
-    def _enable_cors(self):
-        def options_handler(path=None):
-            return
-
-        self._server.install(EnableCors())
-        self._server.route("/", method="OPTIONS", callback=options_handler)
-        self._server.route("/<path:path>", method="OPTIONS", callback=options_handler)
+    def make_json_response(self, status, body, dumps):
+        return web.json_response(body, status=status, dumps=dumps)
