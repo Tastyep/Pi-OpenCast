57526cf9896e87cbf3f05bac9dee9b9fe305a9bd
Luc Sinet <luc.sinet@gmail.com>
2020-07-21 18:46:39 +0200

    api-cleanup #comment Remove the not very useful file controler.
     - Contraint by aiohttp not supporting templates out of the box.

--- OpenCast/app/controller/module.py
+++ OpenCast/app/controller/module.py
@@ -1,11 +1,9 @@
-from .file import FileController
 from .player import PlayerController
 from .player_monitor import PlayerMonitController
 
 
 class ControllerModule:
     def __init__(self, app_facade, infra_facade, data_facade, service_factory):
-        self._file_controller = FileController(app_facade, infra_facade.server)
         self._player_monitor = PlayerMonitController(
             app_facade, infra_facade, data_facade, service_factory
         )
