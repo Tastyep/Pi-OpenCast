bcb549a7a7703be75fdfefc9768a5921b677eae4
Luc Sinet <luc.sinet@gmail.com>
2020-07-22 17:59:34 +0200

    api-cleanup #comment Add IO channels, sends and receive messages like with a pipe.
     - Add Janus dependency for comunicating between synced and asynced threads.

--- OpenCast/infra/io/factory.py
+++ OpenCast/infra/io/factory.py
@@ -1,6 +1,13 @@
+from .channel import JanusChannel, PollingChannel
 from .server import Server
 
 
 class IoFactory:
     def make_server(self, *args):
         return Server(*args)
+
+    def make_polling_channel(self, *args):
+        return PollingChannel(*args)
+
+    def make_janus_channel(self, *args):
+        return JanusChannel(*args)
