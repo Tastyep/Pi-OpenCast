2de62b43a8dae90f7b1337adfcd38348e27bcdd0
Luc Sinet <luc.sinet@gmail.com>
2020-07-22 17:55:45 +0200

    api-cleanup #comment Send response based on observed events.
     For now a 200 is returned on success and a 400 on operation errors.

--- OpenCast/app/controller/monitor.py
+++ OpenCast/app/controller/monitor.py
@@ -1,6 +1,8 @@
+import functools
 import json
 
 from OpenCast.app.tool.json_encoder import ModelEncoder
+from OpenCast.domain.service.identity import IdentityService
 
 from .controller import Controller
 
@@ -12,8 +14,21 @@ class MonitorController(Controller):
         self._base_route = f"/api{base_route}"
         self._json_dumps = functools.partial(json.dumps, cls=ModelEncoder)
 
-    def _route(self, route, *args, **kwargs):
-        self._server.route(f"{self._base_route}{route}", *args, **kwargs)
+    def _observe_dispatch(
+        self, evtcls_handler: dict, cmd_cls, component_id, *args, **kwargs
+    ):
+        cmd_id = IdentityService.id_command(cmd_cls, component_id)
+        self._evt_dispatcher.observe(cmd_id, evtcls_handler, 1)
+        self._cmd_dispatcher.dispatch(cmd_cls(cmd_id, component_id, *args, **kwargs))
+
+    def _route(self, method, route, handle):
+        self._server.route(method, f"{self._base_route}{route}", handle)
+
+    def _ok(self, body=None):
+        return self._make_response(200, body)
+
+    def _bad_request(self):
+        return self._make_response(400, None)
 
     def _make_response(self, status, body):
         return self._server.make_json_response(status, body, self._json_dumps)
