d02f6e761af26af58d8c67ee96d200d4590f579d
Luc Sinet <luc.sinet@gmail.com>
2020-08-14 16:53:58 +0200

    add-video-monitor-controller #comment Add the video monitor controller
     - Exposes get/list/delete operations.
     - Adds related tests.

--- OpenCast/app/controller/module.py
+++ OpenCast/app/controller/module.py
@@ -1,10 +1,14 @@
 from .player import PlayerController
 from .player_monitor import PlayerMonitController
+from .video_monitor import VideoMonitController
 
 
 class ControllerModule:
     def __init__(self, app_facade, infra_facade, data_facade, service_factory):
         self._player_monitor = PlayerMonitController(
             app_facade, infra_facade, data_facade, service_factory
+        )
+        self._video_monitor = VideoMonitController(
+            app_facade, infra_facade, data_facade
         )
         self._player_controller = PlayerController(app_facade, data_facade)
