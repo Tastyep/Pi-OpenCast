a1ffbbffe5fc3157581f8bc294860f63e87fbf80
Luc Sinet <luc.sinet@gmail.com>
2020-08-15 14:07:34 +0200

    add-video-monitor-controller #comment Rebuilds webapp.

---
+++ webapp/build/precache-manifest.ea3bc7214f031d9a86bc3b95c2f9480e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f73988053802838c9a8a578cbb4067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91959702c492e009dc48",
+    "url": "/static/css/main.f31fad5c.chunk.css"
+  },
+  {
+    "revision": "5aadc0bb09d824de7ccc",
+    "url": "/static/js/2.716f499f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.716f499f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91959702c492e009dc48",
+    "url": "/static/js/main.5df12f9c.chunk.js"
+  },
+  {
+    "revision": "b3cfd9f5cdf691e24e2f",
+    "url": "/static/js/runtime-main.00ed32f5.js"
+  }
+]);
