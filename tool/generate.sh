69a7ab5d12d61f659f8562a35f3c6e18397d9efa
Luc Sinet <luc.sinet@gmail.com>
2020-08-24 17:54:12 +0200

    travis-to-gh-actions #comment Split gendoc command into gen and doc commands.

---
+++ tool/generate.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+HERE="$(cd "$(dirname "${BASH_SOURCE:-0}")" && pwd)"
+ROOT="$(cd "$HERE/.." && pwd)"
+
+source "$ROOT/script/gen_cli.sh"
+source "$ROOT/script/env.sh"
+
+#### CLI handlers
+
+function doc() {
+  (cd "$ROOT/docs" && penv make html -b coverage)
+}
+
+#### Internal functions
+
+declare -A COMMANDS
+COMMANDS=(
+  [doc]="Generate documentation files."
+)
+make_basic_cli default_help_display COMMANDS "$@"
