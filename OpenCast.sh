f495e09ca3b99a5583db2ea6e11c06a4d6aa257e
Luc Sinet <luc.sinet@gmail.com>
2020-08-26 14:52:21 +0200

    develop #comment Move the test command to its own script.
     - Expose 3 commands for the test scripts: back, front and all.

--- OpenCast.sh
+++ OpenCast.sh
@@ -1,36 +1,10 @@
 #!/usr/bin/env bash
 
 PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
-PROJECT_NAME="OpenCast"
-
-API_PORT="2020"
-WEBAPP_PORT="8081"
-
-LOG_DIR="log"
-DOC_DIR="docs"
-TEST_DIR="test"
-WEBAPP_DIR="webapp"
-
-LOG_FILE="$PROJECT_NAME.log"
 
 source "$PROJECT_DIR/script/cli_builder.sh"
-source "$PROJECT_DIR/script/env.sh"
 
 #### CLI handlers
-
-function test() {
-  cd "$PROJECT_DIR" || exit 1
-  if [ -z "$1" ]; then
-    penv python -m unittest discover -v
-  else
-    local selector="$1"
-
-    if [[ "$selector" != "$TEST_DIR"* ]]; then
-      selector="$TEST_DIR.$selector"
-    fi
-    penv python -m unittest "$selector"
-  fi
-}
 
 function format() {
   "$PROJECT_DIR/tool/format.sh" "$@"
@@ -48,10 +22,8 @@ function service() {
   "$PROJECT_DIR/tool/service.sh" "$@"
 }
 
-#### Internal functions
-
-function is_port_bound() {
-  lsof -t -a -i ":$1" -c python
+function test() {
+  "$PROJECT_DIR/tool/test.sh" "$@"
 }
 
 #### CLI definition
@@ -62,6 +34,6 @@ COMMANDS=(
   [gen]="Generate content."
   [lint]="Run linters on given targets."
   [service]="Operate services."
-  [test]="Run the test suite."
+  [test]="Run the test suite of a service."
 )
 make_cli default_help_display COMMANDS "$@"
