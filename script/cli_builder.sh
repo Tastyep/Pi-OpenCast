b4c43bd8e464b48770544a63db1cc6871435c14b
Luc Sinet <luc.sinet@gmail.com>
2020-08-30 16:26:59 +0200

    test-coverage #comment Provide --coverage option to test command.
     - Implement more advanced argument parser, differenciate optional from required arguments and options.

--- script/cli_builder.sh
+++ script/cli_builder.sh
@@ -54,27 +54,76 @@ function default_help_display() {
 }
 
 function expect_params() {
-  local -n params_ref
-  local command
-  local args
-  local count
+  local -n params_ref parsed_ref
+  local -a params_attr
+  local command args count
 
   params_ref="$1"
-  command="$2"
+  parsed_ref="$2"
+  command="$3"
   args=("$@")
   count="${#args[@]}"
-  for ((i = 2; i < count; i++)); do
-    local param
 
-    param="${args[$i]}"
-    if ! element_in "$param" "${params_ref[@]}"; then
-      printf "$0 $command: invalid argument $param\n"
-      printf "usage: $command "
-      for param in "$params_ref"; do
-        printf "[$param]"
-      done
-      printf "\n"
-      exit 1
+  parse_params params_attr
+
+  local matched_params
+  matched_params=()
+  # Iterate on arguments
+  for ((i = 3; i < count; i++)); do
+    local arg found index
+
+    arg="${args[$i]}"
+    found=0
+    for j in "${!params_ref[@]}"; do
+      if element_in "$j" "${matched_params[@]}"; then
+        continue
+      fi
+      if [[ "$arg" == "${params_ref["$j"]}" ]]; then
+        found=1
+        index="$j"
+        break
+      fi
+      if [[ "$found" == "0" && "${params_attr["$j"]}" == *"argument"* ]]; then
+        found=1
+        index="$j"
+      fi
+    done
+    if [[ "$found" == "0" ]]; then
+      printf "$0 $command: invalid argument $arg\n"
+      display_help
     fi
+
+    matched_params+=("$index")
+    parsed_ref["${params_ref["$j"]}"]="$arg"
+  done
+  for i in "${!params_ref[@]}"; do
+    if [[ "${params_attr["$i"]}" == *"required"* ]] && ! element_in "$i" "${matched_params[@]}"; then
+      printf "$0 $command: missing required argument ${params_ref["$i"]}\n"
+      display_help
+    fi
+  done
+}
+
+function display_help() {
+  printf "usage: $command"
+  for param in "${params_ref[@]}"; do
+    printf " $param"
+  done
+  printf "\n"
+  exit 1
+}
+
+function parse_params() {
+  local -n attrs_ref
+
+  attrs_ref="$1"
+  for param in "${params_ref[@]}"; do
+    local attr=""
+
+    [[ "$param" =~ \[.*\] ]] && attr="$attr|optional"
+    [[ "$param" =~ \(.*\) ]] && attr="$attr|required"
+    [[ "$param" =~ \<.*\> ]] && attr="$attr|argument"
+    [[ "$param" =~ --.* ]] && attr="$attr|option"
+    attrs_ref+=("$attr")
   done
 }
