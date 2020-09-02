638684664798335bf5bd0958ea53328af9956d5b
Luc Sinet <luc.sinet@gmail.com>
2020-09-02 10:36:36 +0200

    model-to-id-referencing #comment Add tests for media removal.
     - Pass the model id to the event expecter because a cmd targeted to a component can have side effects on other components (VideoDeleted -> VideoRemoved).

--- test/integration/app/service/util.py
+++ test/integration/app/service/util.py
@@ -4,7 +4,9 @@ from test.shared.infra.facade_mock import InfraFacadeMock
 from test.util import TestCase
 from unittest.mock import Mock
 
+from OpenCast.app.controller.module import ControllerModule
 from OpenCast.app.facade import AppFacade
+from OpenCast.app.service.module import ServiceModule
 from OpenCast.domain.service.factory import ServiceFactory
 from OpenCast.infra.data.facade import DataFacade
 from OpenCast.infra.data.repo.factory import RepoFactory
@@ -21,12 +23,27 @@ class ServiceTestCase(TestCase):
 
         repo_factory = RepoFactory()
         self.data_facade = DataFacade(repo_factory)
+        self.data_producer = DataProducer.make()
+        self.data_producer.player().populate(self.data_facade)
 
         infraServiceFactory = Mock()
         self.service_factory = ServiceFactory(infraServiceFactory)
 
+        self.downloader = Mock()
+        self.video_parser = Mock()
         self.infra_facade = InfraFacadeMock()
+        self.infra_facade.media_factory.make_downloader.return_value = self.downloader
+        self.infra_facade.media_factory.make_video_parser.return_value = (
+            self.video_parser
+        )
+
         self.evt_expecter = EventExpecter(
             self.app_facade.cmd_dispatcher, self.app_facade.evt_dispatcher
         )
-        self.data_producer = DataProducer.make()
+
+        ControllerModule(
+            self.app_facade, self.infra_facade, self.data_facade, self.service_factory
+        )
+        ServiceModule(
+            self.app_facade, self.infra_facade, self.data_facade, self.service_factory
+        )
