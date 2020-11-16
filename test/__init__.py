a4341a7beb74b20d0082228aaf2645d90e4238ab
Luc Sinet <luc.sinet@gmail.com>
2020-11-16 20:20:46 +0100

    improve-logging Set the default log level to CRITICAL for tests.
     - Made explicit the possibility to change the level using environment overrides.

--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,13 @@
+import logging
+from os import environ
+
+from OpenCast.config import config
 from OpenCast.infra.log.module import init as init_logging
 
+# Default the log level to critical for tests.
+# Override OPENCAST_LOG_LEVEL to change its value.
+config.load_from_dict({"log": {"level": "CRITICAL"}})
+config.override_from_env(environ, prefix="OPENCAST")
+
 init_logging("OpenCast")
+logging.getLogger("OpenCast").setLevel(config["log.level"])
