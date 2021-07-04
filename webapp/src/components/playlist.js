014ee68b53e4ec9984ab180e2af6fe81ea08e19c
Luc Sinet <luc.sinet@gmail.com>
2021-07-04 18:09:33 +0200

    Toggle the playlist button from pause to play
    Clicking once plays the media, a second time pauses it, then unpauses

--- webapp/src/components/playlist.js
+++ webapp/src/components/playlist.js
@@ -1,121 +1,127 @@
 import React from "react";
 
-import { Divider, IconButton, List, ListItem, ListItemIcon, ListItemText, ListSubheader } from "@material-ui/core";
-import DeleteIcon from '@material-ui/icons/Delete';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import {
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import PauseIcon from "@material-ui/icons/Pause";
 
-import { Droppable, Draggable } from 'react-beautiful-dnd';
+import { Droppable, Draggable } from "react-beautiful-dnd";
 
 import { observer } from "mobx-react-lite";
 
-import playlistAPI from "services/api/playlist"
-import playerAPI from "services/api/player"
+import playlistAPI from "services/api/playlist";
+import playerAPI from "services/api/player";
 import { useAppStore } from "./app_context";
-
 
 // const getListStyle = isDraggingOver => ({
 //   backgorund: isDraggingOver ? 'lightblue' : '#F5F5F5',
 // });
-const getListStyle = isDraggingOver => ({
-  background: isDraggingOver ? 'lightblue' : '#F5F5F5',
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "#F5F5F5",
 });
 const getItemStyle = (isDragging, draggableStyle) => ({
   // styles we need to apply on draggables
   ...draggableStyle,
 
   ...(isDragging && {
-    backgroundColor: "#C5C5C5"
+    backgroundColor: "#C5C5C5",
   }),
   parent: {
-    backgroundColor: 'yellow',
-    '&:hover $child': {
-      color: 'red'
-    }
-
+    backgroundColor: "yellow",
+    "&:hover $child": {
+      color: "red",
+    },
   },
   child: {
-    fontSize: '2em',
-    padding: 24
-  }
-  
+    fontSize: "2em",
+    padding: 24,
+  },
 });
 
 const subheaderStyle = {
-  backgroundColor: "#FFFFFF"
-}
+  backgroundColor: "#FFFFFF",
+};
 
-const Playlist = observer(({playlist}) => {
-  const store = useAppStore()
-  const videos = store.playlistVideos(playlist.id)
-  const playerVideoId = store.player.video_id
+const Playlist = observer(({ playlist }) => {
+  const store = useAppStore();
+  const videos = store.playlistVideos(playlist.id);
+  const player = store.player;
+  const activeVideoId = store.player.videoId;
+  const isPlayerPlaying = player.isPlaying;
 
   const removePlaylist = () => {
-    playlistAPI
-      .delete_(playlist.id)
-      .catch((error) => console.log(error));
-  }
+    playlistAPI.delete_(playlist.id).catch((error) => console.log(error));
+  };
 
-  const playVideo = (video) => {
-    playerAPI
-      .playMedia(video.id)
-  }
+  const onMediaClicked = (media) => {
+    if (media.id !== activeVideoId) {
+      playerAPI.playMedia(media.id).catch((error) => console.log(error));
+      return;
+    }
+    playerAPI.pauseMedia().catch((error) => console.log(error));
+  };
+
+  const renderButtonState = (video) => {
+    if (video.id === activeVideoId) {
+      const icon = isPlayerPlaying ? <PauseIcon /> : <PlayArrowIcon />;
+      return <ListItemIcon>{icon}</ListItemIcon>;
+    }
+  };
 
   return (
     <Droppable droppableId={playlist.id}>
       {(provided, snapshot) => (
         <List
           subheader={
-            <ListSubheader
-              style={subheaderStyle}
-            >
+            <ListSubheader style={subheaderStyle}>
               <b>{playlist.name}</b>
-              { playlist.id !== store.player.queue && 
-                <IconButton
-                  onClick={() => removePlaylist()}
-                >
+              {playlist.id !== store.player.queue && (
+                <IconButton onClick={() => removePlaylist()}>
                   <DeleteIcon />
                 </IconButton>
-              }
+              )}
             </ListSubheader>
           }
           style={getListStyle(snapshot.isDraggingOver)}
           ref={provided.innerRef}
         >
-        { videos.map((video, index) => (
-          <Draggable draggableId={video.id} index={index} key={video.id}>
-            {(provided, snapshot) => (
-            <>
-              <ListItem
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-                style={getItemStyle(
-                  snapshot.isDragging,
-                  provided.draggableProps.style
-                )}
-                button
-                disableRipple
-                onClick={() => { playVideo(video) }}
-              >
-                { video.id === playerVideoId &&
-                  <ListItemIcon>
-                    <PlayArrowIcon />
-                  </ListItemIcon>
-                }
-                <ListItemText primary={video.title} />
-              </ListItem>
-              { index < videos.length - 1 &&
-                <Divider/>
-              }
-            </>  
-          )}
-          </Draggable>
-        ))}
-        {provided.placeholder}
+          {videos.map((video, index) => (
+            <Draggable draggableId={video.id} index={index} key={video.id}>
+              {(provided, snapshot) => (
+                <>
+                  <ListItem
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    style={getItemStyle(
+                      snapshot.isDragging,
+                      provided.draggableProps.style
+                    )}
+                    button
+                    disableRipple
+                    onClick={() => onMediaClicked(video)}
+                  >
+                    {renderButtonState(video)}
+                    <ListItemText primary={video.title} />
+                  </ListItem>
+                  {index < videos.length - 1 && <Divider />}
+                </>
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
         </List>
       )}
     </Droppable>
-  )
-})
+  );
+});
 
-export default Playlist
+export default Playlist;
