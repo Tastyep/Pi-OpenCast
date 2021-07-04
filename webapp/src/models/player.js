6e9d048dd20e6619cc2b494c485d90b8a417d3c9
Luc Sinet <luc.sinet@gmail.com>
2021-07-04 18:08:37 +0200

    Define model's attributes instead of merging response data object.

--- webapp/src/models/player.js
+++ webapp/src/models/player.js
@@ -1,14 +1,27 @@
-import { action, makeObservable, observable } from "mobx"
+import { action, makeObservable, observable, computed } from "mobx";
+
+const PlayerState = {
+  PLAYING: "PLAYING",
+  PAUSED: "PAUSED",
+  STOPPED: "STOPPED",
+};
 
 export default class Player {
   constructor(state, eventDispatcher) {
-    Object.assign(this, state)
+    this.id = state.id;
+    this.queue = state.queue;
+    this.state = state.state;
+    this.videoId = state.video_id;
+    this.subState = state.sub_state;
+    this.subDelay = state.sub_delay;
+    this.volume = state.volume;
 
     makeObservable(this, {
+      queue: observable,
       state: observable,
-      sub_state: observable,
-      sub_delay: observable,
-      video_id: observable,
+      subState: observable,
+      subDelay: observable,
+      videoId: observable,
       volume: observable,
 
       setState: action,
@@ -16,43 +29,50 @@ export default class Player {
       setSubDelay: action,
       setVideoId: action,
       setVolume: action,
-    })
 
-    eventDispatcher.observe({
-      PlayerStarted: (e) => this.onPlayerStarted(e),
-      PlayerStopped: (e) => this.onPlayerStopped(e),
-      PlayerStateToggled: (e) => this.setState(e.state),
-      VolumeUpdated: (e) => this.setVolume(e.volume),
-      SubtitleStateUpdated: (e) => this.setSubState(e.state),
-      SubtitleDelayUpdated: (e) => this.setSubDelay(e.delay),
-    }, this.id)
+      isPlaying: computed,
+    });
+
+    eventDispatcher.observe(
+      {
+        PlayerStarted: (e) => this.onPlayerStarted(e),
+        PlayerStopped: (e) => this.onPlayerStopped(e),
+        PlayerStateToggled: (e) => this.setState(e.state),
+        VolumeUpdated: (e) => this.setVolume(e.volume),
+        SubtitleStateUpdated: (e) => this.setSubState(e.state),
+        SubtitleDelayUpdated: (e) => this.setSubDelay(e.delay),
+      },
+      this.id
+    );
   }
 
   onPlayerStarted(e) {
-    this.setState(e.state)
-    this.setVideoId(e.video_id)
+    this.setState(e.state);
+    this.setVideoId(e.video_id);
   }
 
   onPlayerStopped(e) {
-    this.setState(e.state)
-    this.setVideoId(e.video_id)
+    this.setState(e.state);
+    this.setVideoId(e.video_id);
   }
 
   setState(state) {
-    this.state = state
+    this.state = state;
   }
   setVolume(volume) {
-    this.volume = volume
+    this.volume = volume;
   }
   setSubState(subState) {
-    this.sub_state = subState
+    this.subState = subState;
   }
   setSubDelay(subDelay) {
-    this.sub_delay = subDelay
+    this.subDelay = subDelay;
   }
   setVideoId(videoId) {
-    this.video_id = videoId
+    this.videoId = videoId;
+  }
+
+  get isPlaying() {
+    return this.state === PlayerState.PLAYING;
   }
 }
-
-
--- webapp/src/models/video.js
+++ webapp/src/models/video.js
@@ -1,5 +1,13 @@
 export default class Video {
   constructor(state) {
-    Object.assign(this, state)
+    this.id = state.id;
+    this.source = state.source;
+    this.sourceProtocol = state.source_protocol;
+    this.title = state.title;
+    this.collectionId = state.collection_id;
+    this.thumbnail = state.thumbnail;
+    this.location = state.location;
+    this.streams = state.streams;
+    this.subtitle = state.subtitle;
   }
 }
