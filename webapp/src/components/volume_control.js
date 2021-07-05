dda00ddea1fd5101632abdbacff2eff9adc6369b
Luc Sinet <luc.sinet@gmail.com>
2021-07-05 08:37:05 +0200

    Replace the volume mark with a tooltip

--- webapp/src/components/volume_control.js
+++ webapp/src/components/volume_control.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Slider } from "@material-ui/core";
+import { Grid, Slider, Tooltip } from "@material-ui/core";
 
 import VolumeDown from "@material-ui/icons/VolumeDown";
 import VolumeUp from "@material-ui/icons/VolumeUp";
@@ -8,20 +8,21 @@ import playerAPI from "services/api/player";
 import { useAppStore } from "./app_context";
 import { observer } from "mobx-react-lite";
 
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
+
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
+
 const VolumeControl = observer(() => {
   const store = useAppStore();
 
   const handleCommit = (_, value) => {
     playerAPI.updateVolume(value);
-  };
-
-  const generateMarks = () => {
-    return [
-      {
-        value: store.player.volume,
-        label: store.player.volume + "%",
-      },
-    ];
   };
 
   return (
@@ -33,11 +34,11 @@ const VolumeControl = observer(() => {
         <Grid item xs>
           <Slider
             value={store.player.volume ? store.player.volume : 50}
+            ValueLabelComponent={ValueLabelComponent}
             onChange={(_, value) => {
               store.player.setVolume(value);
             }}
             onChangeCommitted={handleCommit}
-            marks={generateMarks()}
             aria-labelledby="continuous-slider"
           />
         </Grid>
