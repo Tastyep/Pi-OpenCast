e144986c34b27315fffd3358d633b35fbe96a7cb
Luc Sinet <luc.sinet@gmail.com>
2021-07-11 17:06:08 +0200

    Format code using prettier

--- webapp/src/components/playlist.js
+++ webapp/src/components/playlist.js
@@ -9,62 +9,63 @@ import {
   ListItemIcon,
   ListItemText,
   ListSubheader,
-  TextField
+  TextField,
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import PauseIcon from "@material-ui/icons/Pause";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
 
-import {observer} from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 
-import {Draggable, Droppable} from "react-beautiful-dnd";
+import { Draggable, Droppable } from "react-beautiful-dnd";
 
 import playerAPI from "services/api/player";
 import playlistAPI from "services/api/playlist";
-import {useAppStore} from "./app_context";
+import { useAppStore } from "./app_context";
 
 // const getListStyle = isDraggingOver => ({
 //   backgorund: isDraggingOver ? 'lightblue' : '#F5F5F5',
 // });
 const getListStyle = (isDraggingOver) => ({
-  background : isDraggingOver ? "lightblue" : "#F5F5F5",
-  maxHeight : "320px",
-  overflow : "auto",
+  background: isDraggingOver ? "lightblue" : "#F5F5F5",
+  maxHeight: "320px",
+  overflow: "auto",
 });
 const getItemStyle = (isDragging, draggableStyle) => ({
   // styles we need to apply on draggables
   ...draggableStyle,
 
   ...(isDragging && {
-    backgroundColor : "#C5C5C5",
+    backgroundColor: "#C5C5C5",
   }),
 });
 
 const subheaderStyle = {
-  backgroundColor : "#FFFFFF",
+  backgroundColor: "#FFFFFF",
 };
 
 const Playlist = observer(({ playlistId }) => {
   const store = useAppStore();
   const videos = store.playlistVideos(playlistId);
-  const playlist = store.playlists[playlistId]
+  const playlist = store.playlists[playlistId];
   const activeVideoId = store.player.videoId;
   const isPlayerPlaying = store.player.isPlaying;
-  const [name, setName] = useState("")
+  const [name, setName] = useState("");
 
   useEffect(() => {
-    setName(playlist.name)
-  }, [playlist.name])
+    setName(playlist.name);
+  }, [playlist.name]);
 
   const renamePlaylist = (event) => {
     if (!event) {
-      return
+      return;
     }
     event.preventDefault();
-    playlistAPI.update(playlistId, {name: name})
+    playlistAPI
+      .update(playlistId, { name: name })
       .catch((error) => console.log(error));
-    return
-  }
+    return;
+  };
 
   const removePlaylist = () => {
     playlistAPI.delete_(playlistId).catch((error) => console.log(error));
@@ -72,8 +73,9 @@ const Playlist = observer(({ playlistId }) => {
 
   const onMediaClicked = (media) => {
     if (media.id !== activeVideoId) {
-      playerAPI.playMedia(media.id, playlistId)
-          .catch((error) => console.log(error));
+      playerAPI
+        .playMedia(media.id, playlistId)
+        .catch((error) => console.log(error));
       return;
     }
     playerAPI.pauseMedia().catch((error) => console.log(error));
@@ -81,14 +83,14 @@ const Playlist = observer(({ playlistId }) => {
 
   const renderButtonState = (video) => {
     if (video.id === activeVideoId) {
-      const icon = isPlayerPlaying ? <PauseIcon />: <PlayArrowIcon />;
+      const icon = isPlayerPlaying ? <PauseIcon /> : <PlayArrowIcon />;
       return <ListItemIcon>{icon}</ListItemIcon>;
     }
   };
 
   const renderMediaItem = (video, index) => {
     if (!video) {
-      return
+      return;
     }
     return (
       <Draggable draggableId={video.id} index={index} key={video.id}>
@@ -123,22 +125,26 @@ const Playlist = observer(({ playlistId }) => {
         <List
           subheader={
             <ListSubheader style={subheaderStyle}>
-              <form onSubmit={(e) => renamePlaylist(e)} noValidate autoComplete="off">
-              <Grid container>
-                <Grid item xs={11}>
-                  <TextField
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
+              <form
+                onSubmit={(e) => renamePlaylist(e)}
+                noValidate
+                autoComplete="off"
+              >
+                <Grid container>
+                  <Grid item xs={11}>
+                    <TextField
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
                   </Grid>
-                  { store.playlists[playlistId].name !== 'Home' && (
-                <Grid item xs={1}>
-                  <IconButton onClick={() => removePlaylist()}>
-                    <DeleteIcon />
-                  </IconButton>
+                  {store.playlists[playlistId].name !== "Home" && (
+                    <Grid item xs={1}>
+                      <IconButton onClick={() => removePlaylist()}>
+                        <DeleteIcon />
+                      </IconButton>
+                    </Grid>
+                  )}
                 </Grid>
-              )}
-              </Grid>
               </form>
             </ListSubheader>
           }
--- webapp/src/services/dispatcher.js
+++ webapp/src/services/dispatcher.js
@@ -1,20 +1,25 @@
 class SocketEventDispatcher {
   constructor(websockets) {
-    this.eventsToHandlers = {}
-  
+    this.eventsToHandlers = {};
+
     for (const i in websockets) {
-      websockets[i].addEventListener('message', (evt) => this.onEvent(evt))
+      websockets[i].addEventListener("message", (evt) => this.onEvent(evt));
     }
   }
 
   onEvent(event) {
-    const data = JSON.parse(event.data)
-    console.log("Received Event", data.name, this.eventsToHandlers.hasOwnProperty(data.name), event.data)
+    const data = JSON.parse(event.data);
+    console.log(
+      "Received Event",
+      data.name,
+      this.eventsToHandlers.hasOwnProperty(data.name),
+      event.data
+    );
     if (this.eventsToHandlers.hasOwnProperty(data.name)) {
-      const handlers = this.eventsToHandlers[data.name] 
+      const handlers = this.eventsToHandlers[data.name];
       for (const handler of handlers) {
         if (!handler.modelId || data.event.model_id === handler.modelId) {
-          handler.func(data.event)
+          handler.func(data.event);
         }
       }
     }
@@ -23,12 +28,17 @@ class SocketEventDispatcher {
   observe(eventsToHandler, modelId) {
     for (const evt in eventsToHandler) {
       if (this.eventsToHandlers.hasOwnProperty(evt)) {
-        this.eventsToHandlers[evt].push({modelId: modelId, func: eventsToHandler[evt]})
+        this.eventsToHandlers[evt].push({
+          modelId: modelId,
+          func: eventsToHandler[evt],
+        });
       } else {
-        this.eventsToHandlers[evt] = [ {modelId: modelId, func: eventsToHandler[evt]} ]
+        this.eventsToHandlers[evt] = [
+          { modelId: modelId, func: eventsToHandler[evt] },
+        ];
       }
     }
   }
 }
 
-export { SocketEventDispatcher }
+export { SocketEventDispatcher };
