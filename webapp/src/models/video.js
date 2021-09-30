0936a77baaf4d95ff361db8dda5abb22f49521ba
Luc Sinet <luc.sinet@gmail.com>
2021-09-30 15:16:24 +0200

    feat(model): Add new fields to video

--- webapp/src/models/video.js
+++ webapp/src/models/video.js
@@ -4,6 +4,9 @@ export default class Video {
     this.source = state.source;
     this.sourceProtocol = state.source_protocol;
     this.title = state.title;
+    this.duration = state.duration;
+    this.total_playing_duration = state.total_playing_duration;
+    this.last_play = state.last_play;
     this.collectionId = state.collection_id;
     this.thumbnail = state.thumbnail;
     this.location = state.location;
