8cce4c8d41b3367c09bb2450a291ee621739c9b0
Luc Sinet <luc.sinet@gmail.com>
2021-10-02 14:26:45 +0200

    feat: Improve volume component
    Add mute / unmute functions by clicking on the button icon
    Only display the control bar when hovered

--- webapp/src/components/volume_control.js
+++ webapp/src/components/volume_control.js
@@ -1,12 +1,18 @@
-import React from "react";
-import { Grid, Slider, Tooltip } from "@material-ui/core";
-
-import VolumeDown from "@material-ui/icons/VolumeDown";
+import React, { useState } from "react";
+import { Grid, Slider, Tooltip, Button } from "@material-ui/core";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
 import VolumeUp from "@material-ui/icons/VolumeUp";
+import VolumeOff from "@material-ui/icons/VolumeOff";
 
 import playerAPI from "services/api/player";
 import { useAppStore } from "./app_context";
 import { observer } from "mobx-react-lite";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    container: {},
+  })
+);
 
 function ValueLabelComponent(props) {
   const { children, open, value } = props;
@@ -20,20 +26,40 @@ function ValueLabelComponent(props) {
 
 const VolumeControl = observer(() => {
   const store = useAppStore();
+  const [sliderDisplay, setSliderDisplay] = useState(false);
+  const [oldVolume, setOldVolume] = useState(store.player.volume);
+  const classes = useStyles();
 
   const handleCommit = (_, value) => {
     playerAPI.updateVolume(value);
   };
 
+  const handleClick = () => {
+    if (store.player.volume === 0) {
+      store.player.setVolume(oldVolume);
+      playerAPI.updateVolume(oldVolume);
+      return;
+    }
+    setOldVolume(store.player.volume);
+    store.player.setVolume(0);
+    playerAPI.updateVolume(0);
+  };
+
   return (
-    <div>
-      <Grid container spacing={2}>
-        <Grid item>
-          <VolumeDown />
-        </Grid>
-        <Grid item xs>
+    <Grid
+      container
+      onMouseEnter={() => {
+        setSliderDisplay(true);
+      }}
+      onMouseLeave={() => {
+        setSliderDisplay(false);
+      }}
+      className={classes.container}
+    >
+      <Grid item xs={10}>
+        {sliderDisplay && (
           <Slider
-            value={store.player.volume ? store.player.volume : 50}
+            value={store.player.volume}
             ValueLabelComponent={ValueLabelComponent}
             onChange={(_, value) => {
               store.player.setVolume(value);
@@ -41,12 +67,14 @@ const VolumeControl = observer(() => {
             onChangeCommitted={handleCommit}
             aria-labelledby="continuous-slider"
           />
-        </Grid>
-        <Grid item>
-          <VolumeUp />
-        </Grid>
+        )}
       </Grid>
-    </div>
+      <Grid item xs={2}>
+        <Button onClick={handleClick}>
+          {(store.player.volume === 0 && <VolumeOff />) || <VolumeUp />}
+        </Button>
+      </Grid>
+    </Grid>
   );
 });
 
