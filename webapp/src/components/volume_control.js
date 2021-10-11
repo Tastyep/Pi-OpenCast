49387056bddace1abf650857b9d59b547bfb0609
Luc Sinet <luc.sinet@gmail.com>
2021-10-11 09:56:48 +0200

    feat: Improve design of the volume slider

--- webapp/src/components/volume_control.js
+++ webapp/src/components/volume_control.js
@@ -1,22 +1,13 @@
 import React, { useState } from "react";
-import { Grid, Slider, Tooltip, Button } from "@mui/material";
-import { styled } from "@mui/material/styles";
+
+import { Stack, Slider, Tooltip, Button } from "@mui/material";
+import { styled, useTheme } from "@mui/material/styles";
 import VolumeUp from "@mui/icons-material/VolumeUp";
 import VolumeOff from "@mui/icons-material/VolumeOff";
 
 import playerAPI from "services/api/player";
 import { useAppStore } from "./app_context";
 import { observer } from "mobx-react-lite";
-
-const PREFIX = "VolumeControl";
-
-const classes = {
-  container: `${PREFIX}-container`,
-};
-
-const StyledGrid = styled(Grid)(() => ({
-  [`&.${classes.container}`]: {},
-}));
 
 function ValueLabelComponent(props) {
   const { children, open, value } = props;
@@ -30,6 +21,7 @@ function ValueLabelComponent(props) {
 
 const VolumeControl = observer(() => {
   const store = useAppStore();
+  const theme = useTheme();
   const [sliderDisplay, setSliderDisplay] = useState(false);
   const [oldVolume, setOldVolume] = useState(store.player.volume);
 
@@ -49,35 +41,56 @@ const VolumeControl = observer(() => {
   };
 
   return (
-    <StyledGrid
-      container
+    <Stack
+      direction="row"
+      justifyContent="end"
+      alignItems="center"
+      sx={{ minWidth: "160px" }}
       onMouseEnter={() => {
         setSliderDisplay(true);
       }}
       onMouseLeave={() => {
         setSliderDisplay(false);
       }}
-      className={classes.container}
     >
-      <Grid item xs={11}>
-        {sliderDisplay && (
-          <Slider
-            value={store.player.volume}
-            ValueLabelComponent={ValueLabelComponent}
-            onChange={(_, value) => {
-              store.player.setVolume(value);
-            }}
-            onChangeCommitted={handleCommit}
-            aria-labelledby="continuous-slider"
-          />
-        )}
-      </Grid>
-      <Grid item xs={1}>
-        <Button onClick={handleClick}>
-          {(store.player.volume === 0 && <VolumeOff />) || <VolumeUp />}
-        </Button>
-      </Grid>
-    </StyledGrid>
+      {sliderDisplay && (
+        <Slider
+          size="small"
+          value={store.player.volume}
+          ValueLabelComponent={ValueLabelComponent}
+          aria-labelledby="continuous-slider"
+          sx={{
+            color: theme.palette.mode === "dark" ? "#fff" : "rgba(0,0,0,0.87)",
+            "& .MuiSlider-track": {
+              border: "none",
+            },
+            "& .MuiSlider-thumb": {
+              width: 16,
+              height: 16,
+              backgroundColor: "#fff",
+              "&:before": {
+                boxShadow: "0 4px 8px rgba(0,0,0,0.4)",
+              },
+              "&:hover, &.Mui-focusVisible, &.Mui-active": {
+                boxShadow: "none",
+              },
+            },
+          }}
+          onChange={(_, value) => {
+            store.player.setVolume(value);
+          }}
+          onChangeCommitted={handleCommit}
+        />
+      )}
+      <Button
+        sx={{
+          color: theme.palette.mode === "dark" ? "#fff" : "rgba(0,0,0,0.87)",
+        }}
+        onClick={handleClick}
+      >
+        {(store.player.volume === 0 && <VolumeOff />) || <VolumeUp />}
+      </Button>
+    </Stack>
   );
 });
 
