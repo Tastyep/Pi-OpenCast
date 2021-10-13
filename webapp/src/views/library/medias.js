6d10319de40389180e691aef15521dd1b448816d
Luc Sinet <luc.sinet@gmail.com>
2021-10-13 14:36:09 +0200

    feat(MediaList): Implement a layout for small windows
    Export the list from the view to its own component

--- webapp/src/views/library/medias.js
+++ webapp/src/views/library/medias.js
@@ -1,53 +1,15 @@
-import {
-  Avatar,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
-  Stack,
-} from "@mui/material";
-
-import Media from "react-media";
-import { SIZES } from "constants.js";
-
 import { observer } from "mobx-react-lite";
 
-import { duration_to_hms } from "services/duration";
-
 import { useAppStore } from "components/app_context";
-
-const MediaItem = ({ video }) => {
-  return (
-    <ListItem sx={{ width: "100%" }}>
-      <ListItemAvatar>
-        <Avatar alt={video.title} src={video.thumbnail} />
-      </ListItemAvatar>
-      <Stack direction="row" alignItems="center" sx={{ width: "100%" }}>
-        <ListItemText>{video.title}</ListItemText>
-        <Media queries={{ medium: { minWidth: SIZES.medium.min } }}>
-          {(matches) =>
-            matches.medium && <ListItemText> {video.title}</ListItemText>
-          }
-        </Media>
-
-        <ListItemText
-          primary={duration_to_hms(video.duration)}
-          sx={{ textAlign: "right" }}
-        />
-      </Stack>
-    </ListItem>
-  );
-};
+import MediaList from "components/media_list";
 
 const MediasPage = observer(() => {
   const store = useAppStore();
 
   return (
-    <List sx={{ height: "100%", width: "100%" }}>
-      {Object.keys(store.videos).map((videoId, _) => (
-        <MediaItem key={videoId} video={store.videos[videoId]} />
-      ))}
-    </List>
+    <div style={{ height: "100%", width: "100%" }}>
+      <MediaList medias={Object.values(store.videos)} />
+    </div>
   );
 });
 
