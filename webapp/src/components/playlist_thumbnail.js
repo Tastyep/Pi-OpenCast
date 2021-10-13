9795d83c70a0496c831eea7e4dfb6904bc443af4
Luc Sinet <luc.sinet@gmail.com>
2021-10-13 17:07:31 +0200

    feat: Implement page for viewing playlists

---
+++ webapp/src/components/playlist_thumbnail.js
@@ -0,0 +1,53 @@
+import { Box, ImageList, ImageListItem } from "@mui/material";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+
+const listPopularVideos = (videos) => {
+  return videos
+    .sort((a, b) => {
+      return a.total_playing_duration < b.total_playing_duration;
+    })
+    .slice(0, 4);
+};
+
+const PlaylistThumbnail = ({ videos }) => {
+  const popVideos = listPopularVideos(videos);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: 256,
+        width: 256,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#F0F0F0",
+        borderRadius: "5%",
+        overflow: "hidden",
+      }}
+    >
+      {popVideos.length > 0 ? (
+        <ImageList cols={2} gap={0} sx={{ width: "100%", height: "100%" }}>
+          {[0, 1, 2, 3].map((index) => (
+            <ImageListItem key={index}>
+              <img
+                src={popVideos[index % popVideos.length].thumbnail}
+                alt={popVideos[index % popVideos.length].title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      ) : (
+        <PlaylistPlayIcon
+          sx={{
+            width: "50%",
+            height: "50%",
+            color: "#707070",
+          }}
+        />
+      )}
+    </Box>
+  );
+};
+
+export default PlaylistThumbnail;
--- webapp/src/views/library/playlists.js
+++ webapp/src/views/library/playlists.js
@@ -1,13 +1,122 @@
-import {} from "@mui/material";
+import { useState } from "react";
+
+import {
+  Box,
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { styled } from "@mui/material/styles";
+
+import { Link } from "react-router-dom";
 
 import { observer } from "mobx-react-lite";
 
 import { useAppStore } from "components/app_context";
+import playlistAPI from "services/api/playlist";
+import PlaylistThumbnail from "components/playlist_thumbnail";
+
+const ModalContent = styled(Box)({
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  backgroundColor: "#FFFFFF",
+  border: "2px solid #000",
+  boxShadow: 24,
+
+  p: 4,
+  padding: "16px",
+});
+
+const PlaylistItem = ({ playlist }) => {
+  const store = useAppStore();
+
+  return (
+    <ListItem sx={{ flex: 0, flexDirection: "column" }}>
+      <Link
+        to={"/playlists/" + playlist.id}
+        style={{
+          display: "flex",
+        }}
+      >
+        <PlaylistThumbnail videos={store.playlistVideos(playlist.id)} />
+      </Link>
+      <Typography sx={{ marginTop: "16px" }}>{playlist.name}</Typography>
+    </ListItem>
+  );
+};
 
 const PlaylistsPage = observer(() => {
   const store = useAppStore();
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState("");
 
-  return <>{"Playlists"}</>;
+  const createPlaylist = () => {
+    playlistAPI.create({ name: title }).catch((error) => console.log(error));
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <ModalContent>
+          <Typography variant="h6" sx={{ marginBottom: "8px" }}>
+            New playlist
+          </Typography>
+          <TextField
+            id="standard-basic"
+            label="Title"
+            variant="standard"
+            sx={{ width: "100%" }}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+              marginTop: "24px",
+            }}
+          >
+            <Button variant="text" onClick={() => setOpen(false)}>
+              Cancel
+            </Button>
+            <Button variant="contained" onClick={() => createPlaylist()}>
+              Save
+            </Button>
+          </div>
+        </ModalContent>
+      </Modal>
+      <List sx={{ display: "flex", flexWrap: "wrap" }}>
+        <ListItem sx={{ flex: 0, flexDirection: "column" }}>
+          <IconButton
+            sx={{
+              height: "256px",
+              width: "256px",
+              backgroundColor: "#F0F0F0",
+              borderRadius: "5%",
+            }}
+            onClick={() => setOpen(true)}
+          >
+            <AddIcon sx={{ height: "25%", width: "25%" }} />
+          </IconButton>
+          <Typography sx={{ marginTop: "16px" }}>New playlist</Typography>
+        </ListItem>
+        {Object.keys(store.playlists).map((playlistId, _) => (
+          <PlaylistItem
+            key={playlistId}
+            playlist={store.playlists[playlistId]}
+          />
+        ))}
+      </List>
+    </>
+  );
 });
 
 export default PlaylistsPage;
