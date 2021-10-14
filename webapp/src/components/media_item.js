7ac76bd669c78c3be81c91561010dcdb35413be8
Luc Sinet <luc.sinet@gmail.com>
2021-10-14 16:19:21 +0200

    feat: Add option menu on media item
    Menu allows to add the video to a playlist

---
+++ webapp/src/components/media_item.js
@@ -0,0 +1,202 @@
+import { useState } from "react";
+
+import {
+  Avatar,
+  Box,
+  IconButton,
+  Grid,
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Stack,
+  Typography,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+
+import Media from "react-media";
+import { SIZES } from "constants.js";
+
+import { durationToHMS } from "services/duration";
+import playlistAPI from "services/api/playlist";
+import { useAppStore } from "components/app_context";
+
+import PlaylistThumbnail from "components/playlist_thumbnail";
+
+const MediaItem = ({ video }) => {
+  const store = useAppStore();
+
+  const [isHover, setHover] = useState(false);
+  const [anchor, setAnchor] = useState(null);
+  const isMenuOpen = Boolean(anchor);
+
+  const [anchorPl, setAnchorPl] = useState(null);
+  const isPlMenuOpen = Boolean(anchorPl);
+
+  const closeMenu = () => {
+    setAnchor(null);
+  };
+
+  const closePlMenu = () => {
+    setAnchorPl(null);
+  };
+
+  const selectPlaylist = () => {
+    setAnchorPl(anchor);
+    closeMenu();
+  };
+
+  const addToPlaylist = (playlist, video) => {
+    closePlMenu();
+    playlist.ids.push(video.id);
+    playlistAPI.update(playlist.id, { ids: playlist.ids });
+  };
+
+  return (
+    <ListItem
+      sx={{ width: "100%" }}
+      onMouseEnter={() => {
+        setHover(true);
+      }}
+      onMouseLeave={() => {
+        setHover(false);
+      }}
+    >
+      <Media queries={{ large: { minWidth: SIZES.large.min } }}>
+        {(matches) =>
+          matches.large ? (
+            <Grid container>
+              <Grid item xs>
+                <Stack direction="row" alignItems="center">
+                  <ListItemAvatar>
+                    <Avatar alt={video.title} src={video.thumbnail} />
+                  </ListItemAvatar>
+                  <ListItemText>{video.title}</ListItemText>
+                </Stack>
+              </Grid>
+              <Grid item xs alignSelf="center">
+                <ListItemText sx={{ color: "#505050" }}>
+                  {" "}
+                  {"Artist"}
+                </ListItemText>
+              </Grid>
+              <Grid item xs alignSelf="center">
+                <ListItemText sx={{ color: "#505050" }}>
+                  {" "}
+                  {"Album"}
+                </ListItemText>
+              </Grid>
+              <Grid item alignSelf="center">
+                {isHover ? (
+                  <div>
+                    <IconButton
+                      aria-controls="media-menu"
+                      aria-haspopup="true"
+                      aria-expanded={isMenuOpen ? "true" : undefined}
+                      onClick={(e) => setAnchor(e.currentTarget)}
+                    >
+                      <MoreVertIcon />
+                    </IconButton>
+                    <Menu
+                      id="media-menu"
+                      anchorEl={anchor}
+                      open={isMenuOpen}
+                      onClose={closeMenu}
+                      MenuListProps={{
+                        "aria-labelledby": "basic-button",
+                      }}
+                    >
+                      <MenuItem onClick={closeMenu}>
+                        <ListItemIcon>
+                          <PlaylistPlayIcon />
+                        </ListItemIcon>
+                        <ListItemText>Play next</ListItemText>
+                      </MenuItem>
+                      <MenuItem onClick={selectPlaylist}>
+                        <ListItemIcon>
+                          <PlaylistAddIcon />
+                        </ListItemIcon>
+                        <ListItemText>Add to playlist</ListItemText>
+                      </MenuItem>
+                    </Menu>
+                    <Menu
+                      id="playlist-menu"
+                      anchorEl={anchorPl}
+                      open={isPlMenuOpen}
+                      onClose={closePlMenu}
+                      MenuListProps={{
+                        "aria-labelledby": "basic-button",
+                      }}
+                      sx={{ display: "flex" }}
+                    >
+                      <Box>
+                        <ListItem>
+                          <Typography variant="h6" sx={{ fontWeight: 600 }}>
+                            All playlists
+                          </Typography>
+                        </ListItem>
+                        {Object.values(store.playlists).map((playlist) => (
+                          <MenuItem
+                            onClick={() => addToPlaylist(playlist, video)}
+                          >
+                            <div
+                              style={{ width: 64, height: 64, marginRight: 16 }}
+                            >
+                              <PlaylistThumbnail
+                                videos={store.playlistVideos(playlist.id)}
+                              />
+                            </div>
+                            <Stack direction="column">
+                              <ListItemText>{playlist.name}</ListItemText>
+                              <ListItemText>
+                                {store.playlistVideos(playlist.id).length}
+                                medias
+                              </ListItemText>
+                            </Stack>
+                          </MenuItem>
+                        ))}
+                      </Box>
+                    </Menu>
+                  </div>
+                ) : (
+                  <ListItemText
+                    primary={durationToHMS(video.duration)}
+                    sx={{ textAlign: "right", color: "#505050" }}
+                  />
+                )}
+              </Grid>
+            </Grid>
+          ) : (
+            <Grid container alignItems="center">
+              <Grid item xs>
+                <Stack direction="row" alignItems="center">
+                  <ListItemAvatar>
+                    <Avatar alt={video.title} src={video.thumbnail} />
+                  </ListItemAvatar>
+                  <Stack>
+                    <Typography>{video.title}</Typography>{" "}
+                    <Typography sx={{ color: "#505050" }}>
+                      {"Artist • Album"}
+                    </Typography>
+                  </Stack>
+                </Stack>
+              </Grid>
+              <Grid item alignSelf="center">
+                <ListItemText
+                  primary={durationToHMS(video.duration)}
+                  sx={{ textAlign: "right", color: "#505050" }}
+                />
+              </Grid>
+            </Grid>
+          )
+        }
+      </Media>
+    </ListItem>
+  );
+};
+
+export default MediaItem;
