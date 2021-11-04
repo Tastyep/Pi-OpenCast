682da70039d5477f72f1dbb7262585a3fd288f6a
Luc Sinet <luc.sinet@gmail.com>
2021-11-04 11:30:33 +0100

    feat(notif): improve position and size for small devices

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import MediaQuery from "react-responsive";
+import { SIZES } from "constants.js";
+
 import { SnackbarProvider } from "notistack";
 
 import App from "views/app";
@@ -8,18 +11,45 @@ import { AppProvider } from "components/app_context";
 
 import "./index.css";
 
+const SnackBarProviderWrapper = (props) => {
+  const { children } = props;
+
+  return (
+    <MediaQuery maxWidth={SIZES.small.max}>
+      {(matches) =>
+        matches ? (
+          <SnackbarProvider
+            dense
+            maxSnack={2}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+          >
+            {children}
+          </SnackbarProvider>
+        ) : (
+          <SnackbarProvider
+            maxSnack={5}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+          >
+            {children}
+          </SnackbarProvider>
+        )
+      }
+    </MediaQuery>
+  );
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
-      <SnackbarProvider
-        maxSnack={3}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-      >
+      <SnackBarProviderWrapper>
         <App />
-      </SnackbarProvider>
+      </SnackBarProviderWrapper>
     </AppProvider>
   </React.StrictMode>,
   document.getElementById("root")
