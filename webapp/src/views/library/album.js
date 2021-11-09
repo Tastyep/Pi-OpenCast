795e1b9524042dcac3d52ed55d36f3164e3a6432
Luc Sinet <luc.sinet@gmail.com>
2021-11-09 12:29:29 +0100

    feat(album): add same functionalities as for artists
     - Add shuffle button
     - Add menu for queueing and playing next

--- webapp/src/views/library/album.js
+++ webapp/src/views/library/album.js
@@ -1,9 +1,30 @@
-import { Box, Divider, List, Typography } from "@mui/material";
+import { useState } from "react";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import QueueMusicIcon from "@mui/icons-material/QueueMusic";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 import { useParams } from "react-router-dom";
 
 import { observer } from "mobx-react-lite";
 
+import { queueNext, queueLast, shuffleIds } from "services/playlist";
+import playlistAPI from "services/api/playlist";
+import playerAPI from "services/api/player";
+import snackBarHandler from "services/api/error";
 import { humanReadableDuration } from "services/duration";
 
 import { useAppStore } from "components/app_context";
@@ -11,10 +32,107 @@ import MediaItem from "components/media_item";
 
 const pluralize = require("pluralize");
 
+const AlbumMenu = (props) => {
+  const store = useAppStore();
+  const { open, anchorEl, album, closeMenu } = props;
+
+  const playNext = () => {
+    closeMenu();
+
+    const ids = album.videos.map((video) => video.id);
+    const playlistIds = queueNext(
+      store.playerPlaylist,
+      store.player.videoId,
+      ids
+    );
+    playlistAPI
+      .update(store.playerPlaylist.id, { ids: playlistIds })
+      .then((_) => {
+        if (store.player.isStopped) {
+          playerAPI.playMedia(playlistIds[0]).catch(snackBarHandler(store));
+        }
+      })
+      .catch(snackBarHandler(store));
+  };
+
+  const queue = (playlist) => {
+    closeMenu();
+
+    const ids = album.videos.map((video) => video.id);
+    const playlistIds = queueLast(
+      store.playerPlaylist,
+      store.player.videoId,
+      ids
+    );
+    playlistAPI
+      .update(store.playerPlaylist.id, { ids: playlistIds })
+      .then((_) => {
+        store.enqueueSnackbar({
+          message: album.name + " queued",
+          options: {
+            variant: "success",
+          },
+        });
+      })
+      .catch(snackBarHandler(store));
+  };
+
+  return (
+    <Menu
+      id="album-menu"
+      anchorEl={anchorEl}
+      open={open}
+      onClose={closeMenu}
+      MenuListProps={{
+        "aria-labelledby": "basic-button",
+      }}
+      transformOrigin={{ horizontal: "right", vertical: "top" }}
+      anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+    >
+      <MenuItem onClick={() => playNext()}>
+        <ListItemIcon>
+          <PlaylistPlayIcon />
+        </ListItemIcon>
+        <ListItemText>Play next</ListItemText>
+      </MenuItem>
+      <MenuItem onClick={() => queue()}>
+        <ListItemIcon>
+          <QueueMusicIcon />
+        </ListItemIcon>
+        <ListItemText>Add to queue</ListItemText>
+      </MenuItem>
+    </Menu>
+  );
+};
+
 const AlbumPage = observer(() => {
   const store = useAppStore();
   const { name } = useParams();
+
+  const [anchor, setAnchor] = useState(null);
+  const isMenuOpen = Boolean(anchor);
+
   const album = store.albums()[name];
+
+  const shufflePlayNext = () => {
+    const ids = album.videos.map((video) => {
+      return video.id;
+    });
+
+    const playlistIds = queueNext(
+      store.playerPlaylist,
+      store.player.videoId,
+      shuffleIds(ids)
+    );
+    playlistAPI
+      .update(store.playerPlaylist.id, { ids: playlistIds })
+      .then((_) => {
+        if (store.player.isStopped) {
+          playerAPI.playMedia(playlistIds[0]).catch(snackBarHandler(store));
+        }
+      })
+      .catch(snackBarHandler(store));
+  };
 
   const totalDuration = () => {
     let total = 0;
@@ -34,40 +152,66 @@ const AlbumPage = observer(() => {
     <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
           marginTop: "32px",
-          marginBottom: "32px",
+          marginBottom: "24px",
           marginLeft: "16px",
         }}
       >
-        <Box
-          sx={{
-            width: "128px",
-            height: "128px",
-            marginRight: "32px",
-            borderRadius: "8px",
-            overflow: "hidden",
-          }}
-        >
-          <img
-            src={album.thumbnail}
-            alt={name}
-            style={{
-              height: "100%",
-              width: "100%",
-              objectFit: "cover",
+        <Stack direction="row" alignItems="center">
+          <Box
+            sx={{
+              width: "128px",
+              height: "128px",
+              marginRight: "32px",
+              borderRadius: "8px",
+              overflow: "hidden",
             }}
+          >
+            <img
+              src={album.thumbnail}
+              alt={name}
+              style={{
+                height: "100%",
+                width: "100%",
+                objectFit: "cover",
+              }}
+            />
+          </Box>
+          <Box>
+            <Typography variant="h4">{name}</Typography>
+            <Typography>
+              {pluralize("media", album.videos.length, true)}
+            </Typography>
+            <Typography>{totalDuration()}</Typography>
+          </Box>
+        </Stack>
+        <Stack direction="row" sx={{ marginTop: "16px" }}>
+          <Box>
+            <Button
+              disableElevation
+              variant="contained"
+              startIcon={<ShuffleIcon />}
+              sx={{ alignItems: "flex-start", marginRight: "8px" }}
+              onClick={() => shufflePlayNext()}
+            >
+              Shuffle
+            </Button>
+          </Box>
+          <IconButton
+            aria-controls="album-menu"
+            aria-haspopup="true"
+            aria-expanded={isMenuOpen ? "true" : undefined}
+            onClick={(e) => setAnchor(e.currentTarget)}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <AlbumMenu
+            open={isMenuOpen}
+            anchorEl={anchor}
+            album={album}
+            closeMenu={() => setAnchor(null)}
           />
-        </Box>
-        <Box>
-          <Typography variant="h4">{name}</Typography>
-          <Typography>
-            {pluralize("media", album.videos.length, true)}
-          </Typography>
-          <Typography>{totalDuration()}</Typography>{" "}
-        </Box>
+        </Stack>
       </Box>
       <Divider />
       <Box
