86a1ff6c364844f9935eac3a741b7c322e812d7c
Luc Sinet <luc.sinet@gmail.com>
2021-12-10 18:54:08 +0100

    style(ProgressBar): design for both devices layout
     - Only seek the media player when the drag change is committed

--- webapp/src/components/player_progress.js
+++ webapp/src/components/player_progress.js
@@ -15,6 +15,10 @@ const PlayerProgress = observer(() => {
   }
 
   const updateMediaTime = (_, value) => {
+    media.setPlayTime(value);
+  };
+
+  const seekMedia = (_, value) => {
     playerAPI.seekMedia(1000 * value).catch(snackBarHandler(store));
   };
 
@@ -25,6 +29,34 @@ const PlayerProgress = observer(() => {
       max={media.duration}
       value={media.playTime}
       onChange={updateMediaTime}
+      onChangeCommitted={seekMedia}
+      sx={{
+        display: "block",
+        color: "rgba(89,123,157,0.87)",
+        height: 4,
+        padding: "0px",
+        "@media (pointer: coarse)": {
+          padding: "0px",
+        },
+        "& .MuiSlider-thumb": {
+          width: 8,
+          height: 8,
+          transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
+          "&:before": {
+            boxShadow: "0 2px 12px 0 rgba(0,0,0,0.4)",
+          },
+          "&:hover, &.Mui-focusVisible": {
+            boxShadow: `0px 0px 0px 8px ${"rgb(0 0 0 / 16%)"}`,
+          },
+          "&.Mui-active": {
+            width: 20,
+            height: 20,
+          },
+        },
+        "& .MuiSlider-rail": {
+          opacity: 0.28,
+        },
+      }}
     />
   );
 });
