6cded0ea0bbe6567c47ed883358abe32b1a1d858
Luc Sinet <luc.sinet@gmail.com>
2022-10-07 11:51:19 +0200

    style: make player progress slider's thumb squared
     - compute thumb dimension based on bar dimension

--- webapp/src/components/player_progress.js
+++ webapp/src/components/player_progress.js
@@ -22,6 +22,11 @@ const PlayerProgress = observer(({ height = 4 }) => {
     playerAPI.seekMedia(1000 * value).catch(snackBarHandler(store));
   };
 
+  const thumbHeight = height * 2;
+  const thumbWidth = thumbHeight / 2;
+  const activeThumbHeight = thumbHeight + thumbHeight / 2;
+  const activeThumbWidth = thumbWidth + thumbWidth / 2;
+
   return (
     <Slider
       min={0}
@@ -40,18 +45,19 @@ const PlayerProgress = observer(({ height = 4 }) => {
           padding: "0px",
         },
         "& .MuiSlider-thumb": {
-          width: 8,
-          height: 8,
+          width: thumbWidth,
+          height: thumbHeight,
+          borderRadius: "0px",
           transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
           "&:before": {
-            boxShadow: "0 2px 12px 0 rgba(0,0,0,0.4)",
+            boxShadow: "0 2px 8px 0 rgba(0,0,0,0.4)",
           },
           "&:hover, &.Mui-focusVisible": {
             boxShadow: `0px 0px 0px 8px ${"rgb(0 0 0 / 16%)"}`,
           },
           "&.Mui-active": {
-            width: 20,
-            height: 20,
+            width: activeThumbWidth,
+            height: activeThumbHeight,
           },
         },
         "& .MuiSlider-rail": {
